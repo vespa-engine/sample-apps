147ed0564d66e9a9f113911d1e668b90d9cc4cb8
gjoranv <gv@oath.com>
2017-08-23 15:07:08 +0200

    Use 'com.mydomain' instead of 'com.yahoo'

--- basic-search-java/src/test/java/com/mydomain/example/ExampleProcessorTest.java
+++ basic-search-java/src/test/java/com/mydomain/example/ExampleProcessorTest.java
@@ -1,5 +1,5 @@
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-package com.yahoo.example;
+package com.mydomain.example;
 
 import com.yahoo.component.chain.Chain;
 import com.yahoo.processing.Processor;
