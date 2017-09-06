3ee38f8749a9eaee3d031cdd5759657305ff08a6
Jo Kristian Bergum <bergum@yahoo-inc.com>
2017-09-06 15:53:06 +0200

    Simple bootstrap

---
+++ aws_bootstrap.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+## Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+export VESPA_HOME=/opt/vespa
+configserver_addr=$1
+
+if [ $# -eq 0 ]; then
+        configserver_addr=localhost
+fi
+
+echo "Installing Vespa"
+yum -y install yum-utils epel-release
+yum-config-manager --add-repo https://copr.fedorainfracloud.org/coprs/g/vespa/vespa/repo/epel-7/group_vespa-vespa-epel-7.repo
+yum -y install vespa
+
+
+echo "Setting hostname to fqdn '$fqdn'"
+fqdn=$(nslookup $(hostname) |grep Name |awk '{print $2}')
+hostnamectl set-hostname $fqdn
+
+echo "Setting VESPA_CONFIGSERVERS=$configserver_addr"
+echo "override VESPA_CONFIGSERVERS $configserver_addr" >> $VESPA_HOME/conf/vespa/default-env.txt
+
+if [ "$configserver_addr" = "$fqdn" ]; then
+	echo "Staring vespa configuration server "
+	service vespa-configserver start
+fi
+
+echo "Staring vespa services"
+service vespa start;
+
