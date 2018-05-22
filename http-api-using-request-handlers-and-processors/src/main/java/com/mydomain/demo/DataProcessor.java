ff8c396772847dcc50485c29591271e2778eb035
Armando Guereca <armando@teambanjo.com>
2018-05-22 02:57:36 -0700

    Store Document from Processor [NOT WORKING]

--- http-api-using-request-handlers-and-processors/src/main/java/com/mydomain/demo/DataProcessor.java
+++ http-api-using-request-handlers-and-processors/src/main/java/com/mydomain/demo/DataProcessor.java
@@ -1,7 +1,15 @@
 // Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package com.mydomain.demo;
 
+import com.google.inject.Inject;
 import com.yahoo.component.provider.ListenableFreezableClass;
+import com.yahoo.docproc.DocprocService;
+import com.yahoo.docproc.DocumentProcessor.Progress;
+import com.yahoo.docproc.Processing;
+import com.yahoo.docproc.jdisc.DocumentProcessingHandler;
+import com.yahoo.document.Document;
+import com.yahoo.document.DocumentPut;
+import com.yahoo.document.DocumentType;
 import com.yahoo.processing.Processor;
 import com.yahoo.processing.Request;
 import com.yahoo.processing.Response;
@@ -45,9 +53,12 @@ public class DataProcessor extends Processor {
     }
 
     private final DemoComponent termChecker;
+    private final DocumentProcessingHandler docHandler;
 
-    public DataProcessor(DemoComponent termChecker) {
+    @Inject
+    public DataProcessor(DemoComponent termChecker, DocumentProcessingHandler docHandler) {
         this.termChecker = termChecker;
+        this.docHandler = docHandler;
     }
 
     @Override
@@ -81,6 +92,31 @@ public class DataProcessor extends Processor {
                 current = null;
             }
         }
+
+        /*
+        NOTE: As example, we are going to attempt storing a new Document (hardcoded data)
+        On a Real application I assume one should be able of creating/updating any number
+        of documents from here by creating corresponding DocumentOperation(s)
+        */
+
+        DocumentType type = this.docHandler.getDocumentTypeManager().getDocumentType("music");
+        Document document = new Document(type, "id:default:music::10");
+        document.setFieldValue("title", "My Test Title");
+        DocumentPut docPut = new DocumentPut(document);
+
+        Processing proc = com.yahoo.docproc.Processing.of(docPut);
+
+        DocprocService docProcService = this.docHandler.getDocprocServiceRegistry()
+            .getComponent("default");
+        proc.setDocprocServiceRegistry(this.docHandler.getDocprocServiceRegistry());
+
+        Progress progress = docProcService.getExecutor().processUntilDone(proc);
+
+        //
+        // At this point, I'll expect that document is created, but it not, this returns 404:
+        //    curl -v "http://localhost:8080/document/v1/default/music/docid/10"
+        //
+
         return r;
     }
 
---
+++ http-api-using-request-handlers-and-processors/src/main/java/com/mydomain/demo/DemoDocumentProcessor.java
@@ -0,0 +1,12 @@
+package com.mydomain.demo;
+
+import com.yahoo.docproc.SimpleDocumentProcessor;
+import com.yahoo.document.DocumentPut;
+
+public class DemoDocumentProcessor extends SimpleDocumentProcessor {
+
+  @Override
+  public void process(DocumentPut put) {
+    super.process(put);
+  }
+}
