a9f54306c2dabd5f540af16057e93df9d9115aef
Narin Persad <npersad@Narins-MacBook-Pro.local>
2018-12-19 03:42:12 -0500

    adding purchases demo for grouping guide
    - this project can be used for learning along the "Grouping Information in Results"
    - the data is a verbatim from the table at the beginning of the section

---
+++ part-purchases-demo/parts.py
@@ -0,0 +1,22 @@
+import csv
+import argparse
+import json
+import time
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--file", "-f", type=str, required=True)
+args = parser.parse_args()
+
+pattern = '%Y-%m-%d %H:%M:%S'
+
+with open(args.file, mode='r') as infile:
+    reader = csv.DictReader(infile)
+    for idx,row in enumerate(reader):
+        epoch = int(time.mktime(time.strptime(row['date'], pattern)))
+        del row['date']
+        row['date'] = epoch
+        part = {}
+        part['put'] = 'id:purchase:purchase::' + str(idx)
+        part['fields'] = row
+        print json.dumps(part, indent=0, sort_keys=True).replace('\n','')
