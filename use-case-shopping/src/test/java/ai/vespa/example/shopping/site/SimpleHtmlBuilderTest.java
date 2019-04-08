4464070886b9979b38821a9a2bba61f6632f8be3
Lester Solbakken <lesters@oath.com>
2019-04-08 10:47:19 +0200

    Move unit tests into parent package

--- use-case-shopping/src/test/java/ai/vespa/example/shopping/site/SimpleHtmlBuilderTest.java
+++ use-case-shopping/src/test/java/ai/vespa/example/shopping/site/SimpleHtmlBuilderTest.java
@@ -1,7 +1,8 @@
 // Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
-package ai.vespa.example.shopping.site.view;
+package ai.vespa.example.shopping.site;
 
+import ai.vespa.example.shopping.site.view.SimpleHtmlBuilder;
 import org.junit.Test;
 
 import java.util.Map;
--- use-case-shopping/src/test/java/ai/vespa/example/shopping/site/SimpleQueryBuilderTest.java
+++ use-case-shopping/src/test/java/ai/vespa/example/shopping/site/SimpleQueryBuilderTest.java
@@ -1,6 +1,6 @@
 // Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
-package ai.vespa.example.shopping.site.view;
+package ai.vespa.example.shopping.site;
 
 import ai.vespa.example.shopping.site.data.SimpleQueryBuilder;
 import org.junit.Test;
