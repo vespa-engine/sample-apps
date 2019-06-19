744f61428da1996e5982c44ec414312e5ec8f4c9
Jon Marius Venstad <jvenstad@yahoo-inc.com>
2019-06-19 11:58:22 +0200

    Add tests for the pipeline to run

---
+++ basic-search-hosted/src/test/java/com/mydomain/example/ExampleStagingTest.java
@@ -0,0 +1,15 @@
+package com.mydomain.example;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(ai.vespa.hosted.cd.UpgradeTest.class) // This will change to StagingTest again.
+public class ExampleStagingTest {
+
+    @Test
+    public void testNothingYet() {
+
+    }
+
+}
+
---
+++ basic-search-hosted/src/test/java/com/mydomain/example/ExampleSystemTest.java
@@ -0,0 +1,14 @@
+package com.mydomain.example;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(ai.vespa.hosted.cd.FunctionalTest.class) // This will change to SystemTest again.
+public class ExampleSystemTest {
+
+    @Test
+    public void testNothingYet() {
+
+    }
+
+}
