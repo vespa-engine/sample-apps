97d7f5bbd11fea98305d322f95ce03244eaabbcd
Arnstein Ressem <aressem@verizonmedia.com>
2019-07-12 11:02:03 +0200

    Build sample apps in container to be able to control the environment.

---
+++ travis/compile-and-test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+set -e
+
+# The vespaengine/vespa-pipeline contains both JDK 8 and 11. For the sample-apps we
+# must use JDK 11:
+source /etc/profile.d/jdk-env.sh
+/usr/sbin/alternatives --set java "$JAVA_11"
+/usr/sbin/alternatives --set javac "$JAVAC_11"
+export JAVA_HOME="$JAVA_11_HOME"
+
+# Compile and test the sample apps
+mvn --batch-mode --no-snapshot-updates install
+
