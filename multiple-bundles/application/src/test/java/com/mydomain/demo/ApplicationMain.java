7d3c96b7d195153a136004eaeec0100bf1758fde
gjoranv <gv@verizonmedia.com>
2019-11-01 17:04:43 +0100

    Add sample app with two bundles.

---
+++ multiple-bundles/application/src/test/java/com/mydomain/demo/ApplicationMain.java
@@ -0,0 +1,33 @@
+// Copyright 2017 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.mydomain.demo;
+
+import com.yahoo.application.Networking;
+import org.junit.Test;
+
+import java.nio.file.FileSystems;
+import java.util.Objects;
+
+import static org.junit.Assume.assumeTrue;
+
+/**
+ * After running main you can e.g open
+ * http://localhost:8080/search/?query=foo&tracelevel=2
+ * in a browser.
+ */
+public class ApplicationMain {
+
+    @Test
+    public void runFromMaven() throws Exception {
+        assumeTrue(Boolean.valueOf(System.getProperty("isMavenSurefirePlugin")));
+        main(null);
+    }
+
+    public static void main(String[] args) throws Exception {
+        try (com.yahoo.application.Application app = com.yahoo.application.Application.fromApplicationPackage(
+                FileSystems.getDefault().getPath("src/main/application"),
+                Networking.enable)) {
+            Objects.requireNonNull(app);
+            Thread.sleep(Long.MAX_VALUE);
+        }
+    }
+}
---
+++ multiple-bundles/main/src/main/java/com/mydomain/search/FibonacciSearcher.java
@@ -0,0 +1,39 @@
+// Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+package com.mydomain.search;
+
+import com.yahoo.search.*;
+import com.yahoo.search.result.Hit;
+import com.yahoo.search.searchchain.Execution;
+import com.mydomain.lib.FibonacciProducer;
+
+import java.util.logging.Logger;
+
+/**
+ * A searcher using an API from a library bundle in the same application.
+ */
+public class FibonacciSearcher extends Searcher {
+    private static final Logger log = Logger.getLogger(FibonacciSearcher.class.getName());
+
+    private final FibonacciProducer producer;
+
+    private long numCalls = 0;
+
+    public FibonacciSearcher(FibonacciProducer producer) {
+        this.producer = producer;
+    }
+
+    @Override
+    public Result search(Query query, Execution execution) {
+        Result result = execution.search(query); // Pass on to the next searcher to get results
+        Hit hit = new Hit("test");
+        hit.setField("message", "fib(" + numCalls++ + ") = " + producer.getNext());
+        result.hits().add(hit);
+        return result;
+    }
+
+    @Override
+    public void deconstruct() {
+        producer.stop();
+    }
+
+}
