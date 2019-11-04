f08c20a41fca3935e465a271b9c007d77b8876e1
tmartins <thigm85@gmail.com>
2019-11-04 09:40:15 +0100

    get random data from match subset

--- text-search/src/python/collect_training_data.py
+++ text-search/src/python/collect_training_data.py
@@ -3,40 +3,50 @@
 import os
 import sys
 import requests
-from random import sample
 from pandas import DataFrame
-from msmarco import (
-    load_msmarco_queries,
-    load_msmarco_qrels,
-    extract_querie_relevance,
-)
+from msmarco import load_msmarco_queries, load_msmarco_qrels, extract_querie_relevance
 
 QUERIES_FILE_PATH = sys.argv[1]
 RELEVANCE_FILE_PATH = sys.argv[2]
 DATA_FOLDER = sys.argv[3]
 RANK_PROFILE = sys.argv[4]
-OUTPUT_FILE = os.path.join(DATA_FOLDER, "training_data_" + RANK_PROFILE + ".csv")
+NAME = sys.argv[5] if len(sys.argv) > 5 else "match_random"
+OUTPUT_FILE = os.path.join(
+    DATA_FOLDER, "training_data_" + RANK_PROFILE + "_" + NAME + ".csv"
+)
 PROCESSED_QUERIES_FILE = os.path.join(
-    DATA_FOLDER, "training_data_" + RANK_PROFILE + "_processed_queries.csv"
+    DATA_FOLDER, "training_data_" + RANK_PROFILE + "_" + NAME + "_processed_queries.csv"
 )
 
 
-def get_features(url, query, rankprofile, doc_ids=None):
+def create_request_specific_ids(query, rankprofile, doc_ids):
     body = {
-        "yql": "select id from sources * where (userInput(@userQuery));",
+        "yql": "select id, rankfeatures from sources * where rank(([{'ranked': false}]weightedSet(id, "
+        + str({k: 1 for k in doc_ids})
+        + ")), userInput(@userQuery));",
         "userQuery": query,
+        "hits": len(doc_ids),
         "timeout": "15s",
         "presentation.format": "json",
         "ranking": {"profile": rankprofile, "listFeatures": "true"},
     }
-    if doc_ids:
-        body.update(
-            {
-                "yql": "select id, rankfeatures from sources * where (userInput(@userQuery));",
-                "hits": len(doc_ids),
-                "recall": "+(" + " ".join(["id:" + str(doc) for doc in doc_ids]) + ")",
-            }
-        )
+    return body
+
+
+def create_request_top_hits(query, rankprofile, hits):
+
+    body = {
+        "yql": "select id, rankfeatures from sources * where (userInput(@userQuery));",
+        "userQuery": query,
+        "hits": hits,
+        "timeout": "15s",
+        "presentation.format": "json",
+        "ranking": {"profile": rankprofile, "listFeatures": "true"},
+    }
+    return body
+
+
+def get_features(url, body):
 
     r = requests.post(url, json=body)
     if r.status_code != requests.codes.ok:
@@ -60,7 +70,7 @@ def annotate_data(hits, query_id, relevant_id):
     return data
 
 
-def build_dataset(query_relevance, rank_profile, number_random_sample):
+def build_dataset(url, query_relevance, rank_profile, number_random_sample):
     try:
         f_processed = open(PROCESSED_QUERIES_FILE)
         processed_queries = [int(x) for x in f_processed.readlines()]
@@ -73,38 +83,29 @@ def build_dataset(query_relevance, rank_profile, number_random_sample):
         if int(qid) not in processed_queries:
             line += 1
             print("{0}/{1}".format(line, number_queries))
-            retrieved_ids = [
-                hit["fields"]["id"]
-                for hit in get_features(
-                    url="http://localhost:8080/search/",
-                    query=query,
-                    rankprofile=rank_profile,
+
+            relevant_id_request = create_request_specific_ids(
+                query=query, rankprofile=rank_profile, doc_ids=[relevant_id]
+            )
+            hits = get_features(url=url, body=relevant_id_request)
+            if len(hits) != 1 or hits[0]["fields"]["id"] != relevant_id:
+                print("relevant id not found")
+                continue
+
+            random_hits_request = create_request_top_hits(
+                query=query, rankprofile=rank_profile, hits=number_random_sample
+            )
+            hits.extend(get_features(url=url, body=random_hits_request))
+
+            features = annotate_data(hits=hits, query_id=qid, relevant_id=relevant_id)
+            if os.path.isfile(OUTPUT_FILE):
+                DataFrame.from_records(features).to_csv(
+                    OUTPUT_FILE, index=None, mode="a", header=False
                 )
-            ]
-            if relevant_id in retrieved_ids:
-                doc_ids = [relevant_id]
-                if number_random_sample < len(retrieved_ids):
-                    samples = sample(retrieved_ids, number_random_sample)
-                else:
-                    samples = retrieved_ids
-                doc_ids.extend(samples)
-                hits = get_features(
-                    url="http://localhost:8080/search/",
-                    query=query,
-                    rankprofile=rank_profile,
-                    doc_ids=doc_ids,
-                )
-                features = annotate_data(
-                    hits=hits, query_id=qid, relevant_id=relevant_id
-                )
-                if os.path.isfile(OUTPUT_FILE):
-                    DataFrame.from_records(features).to_csv(
-                        OUTPUT_FILE, index=None, mode="a", header=False
-                    )
-                else:
-                    DataFrame.from_records(features).to_csv(OUTPUT_FILE, index=None)
-            with open(PROCESSED_QUERIES_FILE, "a") as f_processed:
-                f_processed.write("{0}\n".format(qid))
+            else:
+                DataFrame.from_records(features).to_csv(OUTPUT_FILE, index=None)
+        with open(PROCESSED_QUERIES_FILE, "a") as f_processed:
+            f_processed.write("{0}\n".format(qid))
 
 
 def main():
@@ -114,6 +115,7 @@ def main():
     query_relevance = extract_querie_relevance(qrels, queries)
 
     build_dataset(
+        url="http://localhost:8080/search/",
         query_relevance=query_relevance,
         rank_profile=RANK_PROFILE,
         number_random_sample=10,
