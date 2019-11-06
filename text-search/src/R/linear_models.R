0c56b41669c9372caa802a1060747f4d00749656
tmartins <thigm85@gmail.com>
2019-11-06 09:54:05 +0100

    train and validate linear models

--- text-search/src/R/linear_models.R
+++ text-search/src/R/linear_models.R
@@ -1,3 +1,4 @@
+library(dplyr)
 library(ggplot2)
 library(reshape2)
 library(arm)
@@ -5,7 +6,7 @@ library(arm)
 #
 # Load data
 #
-data <- read.csv("data/training_data_collectAll.csv")
+data <- read.csv("data/training_data_collectAll_match_random.csv")
 drops <- c("docid", 
            "qid", 
            "relevant", 
@@ -23,82 +24,68 @@ drops <- c("docid",
            "matches.title."
 )
 
-#
-# Select only features to use
-#
-features <- data[, !(names(data) %in% drops)]
-target <- "relevant"
 
 #
-# Lasso
+# Filter queries that would not have been selected by the query
 #
-library(glmnet)
+invalid_queries <- subset(data, bm25.body. == 0 & bm25.title. == 0) %>%
+  select(qid)
+valid_data <- subset(data, !(qid %in% unique(invalid_queries$qid)))
 
-set.seed(1)
-idx <- sample(x = 1:nrow(features), nrow(features), replace = FALSE)
-x=model.matrix(relevant~.,cbind(data.frame(relevant = data$relevant), features))[,-1]
-x <- x[idx,]
-y <- data$relevant[idx]
-
-#grid=10^seq(10,-2,length=100)
-lasso.mod=cv.glmnet(x,y,alpha=1)
-plot(lasso.mod)
-lasso.mod$lambda.min
 #
-# Basic linear models
+# Sample dataset and create train and validation data
 #
-r = glm(formula = as.formula(paste(target, paste(names(features), collapse=" + "), sep=" ~ ")), family = binomial(), data = data)
-r_b = bayesglm(formula = as.formula(paste(target, paste(names(features), collapse=" + "), sep=" ~ ")), family = "binomial", data = data)
+set.seed(42)
+valid_qids <- unique(valid_data$qid)
+number_qid_to_sample <- length(valid_qids)
+sample_qids <- sample(valid_qids, size = number_qid_to_sample, replace = FALSE)
+train_idx <- sample(1:length(sample_qids), floor(length(sample_qids)/2), replace = FALSE)
+sample_train_qids <- sample_qids[train_idx]
+sample_val_qids <- sample_qids[-train_idx]
 
-coefs <- r_b$coefficients
-variables <- names(coefs)
-expression <- as.character(coefs[1])
-for (i in 2:length(r_b$coefficients)){
-  expression <- paste(expression, paste(coefs[i], variables[i], sep = " * "), sep = " + ")
+sample_train_data <- subset(valid_data, qid %in% sample_train_qids)
+sample_train_data <- sample_train_data[!duplicated(sample_train_data), ]
+
+sample_val_data <- subset(valid_data, qid %in% sample_val_qids)
+sample_val_data <- sample_val_data[!duplicated(sample_val_data), ]
+
+#
+# Create function for reciprocal rank
+#
+rr <- function(observed, predicted){
+  sorted_observed <- observed[order(predicted, decreasing = TRUE)]
+  position_relevant <- match(1, sorted_observed)
+  return(1/position_relevant)
 }
 
-r = glm(formula = relevant ~ 1 + bm25.body. + bm25.title., family = binomial(), data = data)
-
-logistic <- function(x){
-  return(1/(1+exp(-x)))
+#
+# Create function to compute validation metrics
+#
+compute_validation_metrics <- function(validation_data, model){
+  unique_queries <- unique(validation_data$qid)
+  rr_values <- vector(mode = "numeric", length = length(unique_queries))
+  count <- 0
+  for (query_id in unique_queries){
+    print(query_id)
+    count <- count + 1
+    x <- subset(validation_data, qid == query_id)
+    predicted <- as.numeric(predict(model, newdata = x))
+    observed <- x$relevant
+    rr_values[count] <- rr(observed = observed, predicted = predicted)
+  }
+  return(rr_values)  
 }
-# data <- data %>% mutate(bm25.body. = scale(bm25.body.),
-#                         bm25.title. = scale(bm25.title.),
-#                         matchCount.body. = scale(matchCount.body.),
-#                         matchCount.title. = scale(matchCount.title.))
 
-drops <- c("docid", 
-           "qid", 
-           "relevant", 
-           "rankingExpression.averagefieldTermMatchBodyFirstPosition.", 
-           "rankingExpression.averagefieldTermMatchTitleFirstPosition.",
-           "rankingExpression.maxfieldTermMatchBodyFirstPosition.",
-           "rankingExpression.maxfieldTermMatchTitleFirstPosition.",
-           "rankingExpression.averagefieldTermMatchBodyOcurrences.",
-           "rankingExpression.maxfieldTermMatchBodyOcurrences.",
-           "rankingExpression.maxfieldTermMatchTitleOcurrences.",
-           "rankingExpression.averagefieldTermMatchTitleOcurrences.",
-           "elementCompleteness.body..elementWeight",
-           "elementCompleteness.title..elementWeight",
-           "matches.body.",
-           "matches.title."
-           )
+#
+# Fit and compute evaluation metric
+#
+r1 = glm(formula = relevant ~ bm25.body. + bm25.title., family = binomial(), data = sample_train_data)
+r1
+r1_rr_values <- compute_validation_metrics(validation_data = sample_val_data, model=r1)
 
-features <- data[, !(names(data) %in% drops)]
+r5 = glm(formula = relevant ~ bm25.body. + bm25.title. + nativeRank.body. + nativeRank.title., family = binomial(), data = sample_train_data)
+r5
+r5_rr_values <- compute_validation_metrics(validation_data = sample_val_data, model=r5)
 
-# cleared <- c("bm25.body.",
-#              "bm25.title.",
-#              "matchCount.body.",
-#              "matchCount.title.")
-# 
-# features <- features[, !(names(features) %in% cleared)]
-
-# features <- features[, names(features)[61:]]
-             
-melted <- melt(data = features)
-
-ggplot(melted) + geom_boxplot(aes(x = variable, y = value))
-
-
-
-as.character(unique(melted[melted["value"] > 12, "variable"]))
+summary(r1_rr_values)
+summary(r5_rr_values)
