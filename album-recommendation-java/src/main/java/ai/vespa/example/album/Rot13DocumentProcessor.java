9cd1969983f0c20676a982270fbcfe457836b0a7
kkraune <kristian@ymail.com>
2019-11-14 13:58:53 +0100

    package name

--- album-recommendation-java/src/main/java/ai/vespa/example/album/Rot13DocumentProcessor.java
+++ album-recommendation-java/src/main/java/ai/vespa/example/album/Rot13DocumentProcessor.java
@@ -1,5 +1,5 @@
 // Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-package com.mydomain.example;
+package ai.vespa.example.album;
 
 import com.yahoo.docproc.DocumentProcessor;
 import com.yahoo.docproc.Processing;
--- album-recommendation-java/src/test/java/ai/vespa/example/album/Rot13DocumentProcessorTest.java
+++ album-recommendation-java/src/test/java/ai/vespa/example/album/Rot13DocumentProcessorTest.java
@@ -1,5 +1,5 @@
 // Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-package com.mydomain.example;
+package ai.vespa.example.album;
 
 import com.yahoo.application.ApplicationBuilder;
 import com.yahoo.application.Networking;
