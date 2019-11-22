df893d60a73f300317b4e438c242ce926fae3aae
tmartins <thigm85@gmail.com>
2019-11-22 13:22:14 +0100

    update R files

--- text-search/src/R/compare_rr.R
+++ text-search/src/R/compare_rr.R
@@ -1,17 +1,5 @@
 library(ggplot2)
 
-data_folder <- "msmarco/"
-
-rr_bm25 <- read.delim(file = file.path(data_folder, "test-output-bm25.tsv"), col.names = c("rr"))
-rr_native <- read.delim(file = file.path(data_folder, "test-output-default.tsv"), col.names = c("rr"))
-
-summary(rr_bm25)
-summary(rr_native)
-
-data <- rbind(data.frame(rr = rr_bm25, ranking = "bm25"), data.frame(rr = rr_native, ranking = "nativeRank"))
-
-ggplot(data) + geom_boxplot(aes(x = ranking, y = rr))
-
 data_folder <- "data/dev_test_data/"
 
 rr_bm25 <- read.delim(file = file.path(data_folder, "text-search-tf-rank-docs-100_bm25_rr.tsv"), col.names = c("qid", "rr"))
