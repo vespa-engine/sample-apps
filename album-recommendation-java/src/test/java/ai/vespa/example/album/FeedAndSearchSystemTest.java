7c3b064dbe88473b7d5bfa661ea108d7ffb23c81
kkraune <kristian@ymail.com>
2019-11-29 14:23:51 +0100

    more explicit

--- album-recommendation-java/src/test/java/ai/vespa/example/album/FeedAndSearchSystemTest.java
+++ album-recommendation-java/src/test/java/ai/vespa/example/album/FeedAndSearchSystemTest.java
@@ -7,11 +7,9 @@ import ai.vespa.hosted.cd.StagingTest;
 import ai.vespa.hosted.cd.TestRuntime;
 import com.yahoo.slime.JsonDecoder;
 import com.yahoo.slime.Slime;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.net.URI;
-import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Map;
 
 import static java.net.http.HttpRequest.BodyPublishers.ofString;
@@ -21,28 +19,26 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SystemTest
 @StagingTest
-@DisplayName("Test that the deployment")
 class FeedAndSearchSystemTest {
 
     private final Endpoint endpoint = TestRuntime.get().deploymentToTest().endpoint("default");
 
     @Test
-    @DisplayName("makes fed documents searchable")
     void feedAndSearch() {
-        var documentPath = "/document/v1/my-music/music/docid/Got-to-be-there";
-        var document = "{\n"
+        String documentPath = "/document/v1/mynamespace/music/docid/Got-to-be-there";
+        String document = "{\n"
                      + "    \"fields\": {\n"
                      + "         \"album\": \"Got to be there\"\n"
                      + "    }\n"
                      + "}\n";
 
-        var yql = "SELECT * FROM SOURCES * WHERE album CONTAINS \"Got to be there\";";
+        String yql = "SELECT * FROM SOURCES * WHERE album CONTAINS \"Got to be there\";";
 
-        var deleteResult = endpoint.send(endpoint.request(documentPath).DELETE());
+        HttpResponse<String> deleteResult = endpoint.send(endpoint.request(documentPath).DELETE());
         assertEquals(200, deleteResult.statusCode());
 
         // the first query needs a higher timeout than the default 500ms, to warm up the code
-        var emptyResult = endpoint.send(endpoint.request("/search/", Map.of("yql", yql,
+        HttpResponse<String> emptyResult = endpoint.send(endpoint.request("/search/", Map.of("yql", yql,
                                                                             "timeout", "5s")));
         
         var emptyInspector = new JsonDecoder().decode(new Slime(), emptyResult.body().getBytes(UTF_8)).get();
@@ -50,11 +46,11 @@ class FeedAndSearchSystemTest {
         assertTrue(emptyInspector.field("root").field("fields").field("totalCount").valid());
         assertEquals(0, emptyInspector.field("root").field("fields").field("totalCount").asLong());
 
-        var feedResult = endpoint.send(endpoint.request(documentPath)
+        HttpResponse<String> feedResult = endpoint.send(endpoint.request(documentPath)
                                                .POST(ofString(document)));
         assertEquals(200, feedResult.statusCode());
 
-        var searchResult = endpoint.send(endpoint.request("/search/", Map.of("yql", yql)));
+        HttpResponse<String> searchResult = endpoint.send(endpoint.request("/search/", Map.of("yql", yql)));
         var searchInspector = new JsonDecoder().decode(new Slime(), searchResult.body().getBytes(UTF_8)).get();
         assertEquals(200, searchResult.statusCode());
         assertTrue(searchInspector.field("root").field("fields").field("totalCount").valid());
