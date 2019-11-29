a3fcf7643d444ca4bb094627863c1e2d0fec71de
kkraune <kristian@ymail.com>
2019-11-29 14:55:40 +0100

    replace Slime with ObjectMapper

--- album-recommendation-java/src/test/java/ai/vespa/example/album/FeedAndSearchSystemTest.java
+++ album-recommendation-java/src/test/java/ai/vespa/example/album/FeedAndSearchSystemTest.java
@@ -5,17 +5,15 @@ import ai.vespa.hosted.cd.Endpoint;
 import ai.vespa.hosted.cd.SystemTest;
 import ai.vespa.hosted.cd.StagingTest;
 import ai.vespa.hosted.cd.TestRuntime;
-import com.yahoo.slime.JsonDecoder;
-import com.yahoo.slime.Slime;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.net.http.HttpResponse;
 import java.util.Map;
 
 import static java.net.http.HttpRequest.BodyPublishers.ofString;
-import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SystemTest
 @StagingTest
@@ -24,7 +22,7 @@ class FeedAndSearchSystemTest {
     private final Endpoint endpoint = TestRuntime.get().deploymentToTest().endpoint("default");
 
     @Test
-    void feedAndSearch() {
+    void feedAndSearch() throws IOException {
         String documentPath = "/document/v1/mynamespace/music/docid/Got-to-be-there";
         String document = "{\n"
                      + "    \"fields\": {\n"
@@ -40,21 +38,18 @@ class FeedAndSearchSystemTest {
         // the first query needs a higher timeout than the default 500ms, to warm up the code
         HttpResponse<String> emptyResult = endpoint.send(endpoint.request("/search/", Map.of("yql", yql,
                                                                             "timeout", "5s")));
-        
-        var emptyInspector = new JsonDecoder().decode(new Slime(), emptyResult.body().getBytes(UTF_8)).get();
         assertEquals(200, emptyResult.statusCode());
-        assertTrue(emptyInspector.field("root").field("fields").field("totalCount").valid());
-        assertEquals(0, emptyInspector.field("root").field("fields").field("totalCount").asLong());
+        assertEquals(0, new ObjectMapper().readTree(emptyResult.body())
+                .get("root").get("fields").get("totalCount").asLong());
 
         HttpResponse<String> feedResult = endpoint.send(endpoint.request(documentPath)
                                                .POST(ofString(document)));
         assertEquals(200, feedResult.statusCode());
 
         HttpResponse<String> searchResult = endpoint.send(endpoint.request("/search/", Map.of("yql", yql)));
-        var searchInspector = new JsonDecoder().decode(new Slime(), searchResult.body().getBytes(UTF_8)).get();
         assertEquals(200, searchResult.statusCode());
-        assertTrue(searchInspector.field("root").field("fields").field("totalCount").valid());
-        assertEquals(1, searchInspector.field("root").field("fields").field("totalCount").asLong());
+        assertEquals(1, new ObjectMapper().readTree(searchResult.body())
+                .get("root").get("fields").get("totalCount").asLong());
     }
 
 }
