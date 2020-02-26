e690210917947f7363323f19996d04c852231e9a
kkraune <kristian@ymail.com>
2020-02-26 12:41:48 +0100

    draft doc search

---
+++ vespa-documentation-search/src/test/java/ai/vespa/cloud/docsearch/VespaDocStagingTest.java
@@ -0,0 +1,14 @@
+package ai.vespa.cloud.docsearch;
+
+import ai.vespa.hosted.cd.StagingTest;
+import org.junit.jupiter.api.Test;
+
+@StagingTest
+public class VespaDocStagingTest {
+
+    @Test
+    public void testSearchAndFeeding() throws Exception {
+
+    }
+
+}
---
+++ vespa-documentation-search/src/test/java/ai/vespa/cloud/docsearch/VespaDocSystemTest.java
@@ -0,0 +1,14 @@
+package ai.vespa.cloud.docsearch;
+
+import ai.vespa.hosted.cd.SystemTest;
+import org.junit.jupiter.api.Test;
+
+@SystemTest
+public class VespaDocSystemTest {
+
+    @Test
+    public void testSearchAndFeeding() throws Exception {
+
+    }
+
+}
