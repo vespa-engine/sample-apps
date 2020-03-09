35a27e34b045d22cda7901e8e906ecfa41e3b0a2
tmartins <thigm85@gmail.com>
2020-03-09 09:20:25 +0100

    label axis

--- text-search/src/R/explore_bm25_embeddings.R
+++ text-search/src/R/explore_bm25_embeddings.R
@@ -2,6 +2,25 @@ library(ggplot2)
 
 data <- read.csv("data/msmarco/train_test_set/training_data_weakAND_title_body_bert_collect_rank_features_embeddings_99_random_samples.csv")
 
+
+ggplot(data, aes(x=bm25.title. + bm25.body., y = stat(count) / sum(count))) + 
+  geom_histogram(fill = "blue", bins=100, alpha=0.5) + 
+  geom_histogram(data = subset(data, relevant == 1), fill = "red", bins=100, alpha = 0.5) +
+  labs(x = "bm25(title) + bm25(body)", y = "Document frequency")
+
+ggplot(data, aes(x=rankingExpression.dot_product_title_bert. + rankingExpression.dot_product_body_bert., y = stat(count) / sum(count))) + 
+  geom_histogram(fill = "blue", bins=100, alpha=0.5) + 
+  geom_histogram(data = subset(data, relevant == 1), fill = "red", bins=100, alpha = 0.5) +
+  labs(x = "dot-product(title, query) + dot-product(body, query)", y = "Document frequency")
+
+ggplot(data, aes(x=rankingExpression.dot_product_title_bert. + rankingExpression.dot_product_body_bert., 
+                 y=bm25.title. + bm25.body.)) + 
+  geom_point(alpha = 0.2) + 
+  geom_point(data = subset(data, relevant == 1), colour = "red") + 
+  labs(x = "dot-product(title, query) + dot-product(body, query)", y = "bm25(title) + bm25(body)")
+
+
+
 ggplot(data) + geom_histogram(aes(x=rankingExpression.dot_product_title_bert.), fill = "white", colour="black")
 ggplot(data) + geom_histogram(aes(x=rankingExpression.dot_product_body_bert.), fill = "white", colour="black")
 
@@ -16,9 +35,6 @@ ggplot(data, aes(x=bm25.title., y = stat(count) / sum(count))) +
   geom_histogram(fill = "blue", bins=100, alpha=0.5) + 
   geom_histogram(data = subset(data, relevant == 1), fill = "red", bins=100, alpha = 0.5)
 
-ggplot(data, aes(x=bm25.title. + bm25.body., y = stat(count) / sum(count))) + 
-  geom_histogram(fill = "blue", bins=100, alpha=0.5) + 
-  geom_histogram(data = subset(data, relevant == 1), fill = "red", bins=100, alpha = 0.5)
 
 ggplot(data, aes(x=rankingExpression.dot_product_title_bert., y = stat(count) / sum(count))) + 
   geom_histogram(fill = "blue", bins=100, alpha=0.5) + 
@@ -28,14 +44,6 @@ ggplot(data, aes(x=rankingExpression.dot_product_body_bert., y = stat(count) / s
   geom_histogram(fill = "blue", bins=100, alpha=0.5) + 
   geom_histogram(data = subset(data, relevant == 1), fill = "red", bins=100, alpha = 0.5)
 
-ggplot(data, aes(x=rankingExpression.dot_product_title_bert. + rankingExpression.dot_product_body_bert., y = stat(count) / sum(count))) + 
-  geom_histogram(fill = "blue", bins=100, alpha=0.5) + 
-  geom_histogram(data = subset(data, relevant == 1), fill = "red", bins=100, alpha = 0.5)
-
-ggplot(data, aes(x=rankingExpression.dot_product_title_bert. + rankingExpression.dot_product_body_bert., 
-                 y=bm25.title. + bm25.body.)) + 
-  geom_point(alpha = 0.2) + 
-  geom_point(data = subset(data, relevant == 1), colour = "red")
 
 ggplot(subset(data, bm25.title. > 0)) + 
   geom_point(aes(x=rankingExpression.dot_product_title_bert., y=bm25.title.), alpha = 0.2) + 
