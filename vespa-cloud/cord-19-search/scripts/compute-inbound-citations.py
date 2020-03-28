77ee96325a21a744594c004cb295cfd2759cf9f4
Jo Kristian Bergum <bergum@yahoo-inc.com>
2020-03-28 16:28:16 +0100

    Revert "More small fixes"

--- vespa-cloud/cord-19-search/scripts/compute-inbound-citations.py
+++ vespa-cloud/cord-19-search/scripts/compute-inbound-citations.py
@@ -5,46 +5,28 @@ import json
 DATA_FILE = sys.argv[1]
 
 def nor(s):
-  return re.sub(r' +', " ", re.sub(r'[0-9!"#$%&()*+,./:;<=>?@[\]^_`{|}~\'—–-]+', "", s or "")).lower().strip()
+  if not s: s = "nope"
+  return re.sub(r'[0-9!"#$%&()*+,-./:;<=>?@[\]^_`{|}~]+', "", s).lower()
 
 duplicates = 0
 docs = {}
 with open(DATA_FILE, 'r') as f:
   for doc in json.load(f):
-    title = nor(doc['title'])
-    if title in docs.keys():
+    if nor(doc['title']) in docs.keys():
       #print("Duplicate title %s" % doc['title'], file=sys.stderr)
-      docs[title].append(doc)
       duplicates = duplicates + 1
-    else:
-      docs[title] = [doc]
+    docs[nor(doc['title'])] = doc
     doc['cited_by'] = []
 print("Total duplicates %d" % duplicates, file=sys.stderr)
 
 inbound = 0
-for ds in docs.values():
-  for doc in ds:
-    for ref in doc['bib_entries']:
-      title = nor(ref['title'])
-      if title in docs.keys():
-        for cited in docs[title]:
-          cited['cited_by'].append(doc['id'])
-        inbound = inbound + 1
-      else:
-        matches = []
-        for key in docs.keys():
-          if title.startswith(key):
-            matches.append(key)
-        matches.sort(key=lambda m: len(m), reverse=True)
-        if len(matches) > 0:
-          for cited in docs[matches[0]]:
-            cited['cited_by'].append(doc['id'])
-          inbound = inbound + 1
-
+for title in docs.keys():
+  doc = docs[title]
+  for ref in doc['bib_entries']:
+    if nor(ref['title']) in docs.keys():
+      docs[nor(ref['title'])]['cited_by'].append(doc['id'])
+      inbound = inbound + 1
 print("Total inbound hits %d" % inbound, file=sys.stderr)
 
-to_print = []
-for ds in docs.values(): to_print.extend(ds)
-
-print(json.dumps(to_print))
+print(json.dumps(list(docs.values())))
 
