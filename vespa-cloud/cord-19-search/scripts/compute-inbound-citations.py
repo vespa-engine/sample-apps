5d47ecc137ee625e15944bb84e2770f33812f954
Jon Marius Venstad <venstad@gmail.com>
2020-03-28 21:13:04 +0100

    Let refs point to all duplicates of a title, and do not collapse these

--- vespa-cloud/cord-19-search/scripts/compute-inbound-citations.py
+++ vespa-cloud/cord-19-search/scripts/compute-inbound-citations.py
@@ -5,28 +5,36 @@ import json
 DATA_FILE = sys.argv[1]
 
 def nor(s):
-  if not s: s = "nope"
-  return re.sub(r'[0-9!"#$%&()*+,-./:;<=>?@[\]^_`{|}~]+', "", s).lower()
+  return re.sub(r' +', " ", re.sub(r'[0-9!"#$%&()*+,./:;<=>?@[\]^_`{|}~\'—–-]+', "", s or "")).lower().strip()
 
 duplicates = 0
 docs = {}
 with open(DATA_FILE, 'r') as f:
   for doc in json.load(f):
-    if nor(doc['title']) in docs.keys():
+    title = nor(doc['title'])
+    if title in docs.keys():
       #print("Duplicate title %s" % doc['title'], file=sys.stderr)
+      docs[title].append(doc)
       duplicates = duplicates + 1
-    docs[nor(doc['title'])] = doc
+    else:
+      docs[title] = [doc]
     doc['cited_by'] = []
 print("Total duplicates %d" % duplicates, file=sys.stderr)
 
 inbound = 0
-for title in docs.keys():
-  doc = docs[title]
-  for ref in doc['bib_entries']:
-    if nor(ref['title']) in docs.keys():
-      docs[nor(ref['title'])]['cited_by'].append(doc['id'])
-      inbound = inbound + 1
+for ds in docs.values():
+  for doc in ds:
+    for ref in doc['bib_entries']:
+      title = nor(ref['title'])
+      if title in docs.keys():
+        for cited in docs[title]:
+          cited['cited_by'].append(doc['id'])
+        inbound = inbound + 1
+
 print("Total inbound hits %d" % inbound, file=sys.stderr)
 
-print(json.dumps(list(docs.values())))
+to_print = []
+for ds in docs.values(): to_print.extend(ds)
+
+print(json.dumps(to_print))
 
