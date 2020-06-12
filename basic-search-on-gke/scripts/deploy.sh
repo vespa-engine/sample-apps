79485de3ca0eb755b1698fcbe6528902e3e4e8bd
Thomas Griseau <tgriseau@gmail.com>
2020-06-12 18:40:39 +0200

    Adding all scripts and config files to run Vespa on GKE

---
+++ basic-search-on-gke/scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Copyright 2018 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+set -euo pipefail
+set -x
+
+kubectl apply -f deployments/configmap.yml -f deployments/master.yml -f deployments/container.yml -f deployments/content.yml -f deployments/headless.yml -f deployments/service.yml
+while [[ $(kubectl get pods -l app=vespa -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}' | sort -u) != "True" ]]; do echo "waiting for pod" && sleep 10; done
+kubectl cp src/main/application/hosts.xml vespa-0:/workspace
+kubectl cp src/main/application/services.xml vespa-0:/workspace
+kubectl cp src/main/application/searchdefinitions vespa-0:/workspace
+kubectl exec vespa-0 -- bash -c '/opt/vespa/bin/vespa-deploy prepare /workspace && /opt/vespa/bin/vespa-deploy activate'
+kubectl exec vespa-0 -- bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:8080/ApplicationStatus)" != "200" ]]; do sleep 10; done'
---
+++ basic-search-on-gke/scripts/feed.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Copyright 2018 Yahoo Holdings. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+set -euo pipefail
+set -x
+
+HOSTNAME=$(kubectl get service/vespa -o jsonpath='{.status.loadBalancer.ingress[*].ip}')
+PORT=$(kubectl get service/vespa -o jsonpath='{.spec.ports[?(@.name=="container")].port}')
+
+curl -s -L -H "Content-Type:application/json" --data-binary @music-data-1.json "http://$HOSTNAME:$PORT/document/v1/music/music/docid/1" | python -m json.tool
+curl -s -L -H "Content-Type:application/json" --data-binary @music-data-2.json "http://$HOSTNAME:$PORT/document/v1/music/music/docid/2" | python -m json.tool
+
