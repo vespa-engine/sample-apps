135d2cd87bc2145c976396e42df7f723f558842f
kkraune <kristian@ymail.com>
2020-06-25 09:51:41 +0200

    better names

--- vespa-cloud/cord-19-search/scripts/wash.py
+++ vespa-cloud/cord-19-search/scripts/wash.py
@@ -6,19 +6,19 @@ def nor(s):
   return re.sub(r' +', " ", re.sub(r'[0-9!"#$%&()*+,./:;<=>?@[\]^_`{|}~\'—–-]+', "", s or "")).lower().strip()
 
 DATA_FILE = sys.argv[1]
-BLACKLIST_FILE = sys.argv[2]
+BLOCKLIST_FILE = sys.argv[2]
 
-blacklist = {}
-with open(BLACKLIST_FILE, 'r') as b:
+blocklist = {}
+with open(BLOCKLIST_FILE, 'r') as b:
   for line in b:
-    blacklist[line.strip()] = None
+    blocklist[line.strip()] = None
 
 duplicates = []
 docs = {}
 with open(DATA_FILE, 'r') as f:
   for doc in json.load(f):
     title = nor(doc['title'])
-    if not title in blacklist.keys() and not title == "":
+    if not title in blocklist.keys() and not title == "":
       if title in docs.keys():
         docs[title].append(doc)
         if not title in duplicates:
@@ -32,4 +32,3 @@ to_print = []
 for ds in docs.values(): to_print.extend(ds)
 
 print(json.dumps(to_print))
-
