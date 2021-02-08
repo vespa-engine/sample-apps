a5f42e4306e3b62dcaa0c86d2760d69cd6123139
Herve Nicol <herve.nicol@gmail.com>
2021-02-08 16:51:45 +0100

    basic-search-on-docker-swarm: fix scripts for python3

--- basic-search-on-docker-swarm/scripts/deploy.sh
+++ basic-search-on-docker-swarm/scripts/deploy.sh
@@ -6,7 +6,7 @@ set -x
 readonly MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $MYDIR
 
-readonly SID=$(tar -C ../src/main/application -cf - . | gzip | curl -s -L --header "Content-Type: application/x-gzip" --data-binary @- "$(hostname):19071/application/v2/tenant/default/session" | python -c "import sys,json; print json.load(sys.stdin)['session-id'];")
+readonly SID=$(tar -C ../src/main/application -cf - . | gzip | curl -s -L --header "Content-Type: application/x-gzip" --data-binary @- "$(hostname):19071/application/v2/tenant/default/session" | python -c "import sys,json; print(json.load(sys.stdin)['session-id']);")
 curl -s -L -X PUT "$(hostname):19071/application/v2/tenant/default/session/$SID/prepared" | python -mjson.tool
 curl -s -L -X PUT "$(hostname):19071/application/v2/tenant/default/session/$SID/active" | python -mjson.tool
 
