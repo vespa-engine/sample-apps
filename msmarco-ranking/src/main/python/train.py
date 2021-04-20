9875d733d04916e37f4b3e07760e61cb7cb80f52
Jo Kristian Bergum <bergum@oath.com>
2021-04-20 12:25:55 +0200

    Add comments

--- msmarco-ranking/src/main/python/train.py
+++ msmarco-ranking/src/main/python/train.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+#This is an example of how to train a LTR model using Lighgbm
 
 import lightgbm as lgb
 import numpy
