bfaaf0b5ad76c47d62d5213b45c81326532dab28
Jo Kristian Bergum <bergum@oath.com>
2021-04-21 12:26:59 +0200

    add utility to dump qrels from ir_dataset

---
+++ msmarco-ranking/src/main/python/dump_passage_dev_qrels.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+import ir_datasets
+
+
+dataset = ir_datasets.load('msmarco-passage/dev/small')
+with open('qrels.dev.small.tsv', 'w') as fp:
+    for query_id,doc_id,relevance,iteration in dataset.qrels_iter():
+        fp.write("{}\t0\t{}\t1\n".format(query_id,doc_id))
