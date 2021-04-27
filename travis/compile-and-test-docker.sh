91564115c309082b2345765eb4f751d02a8aeb15
Kristian Aune <kraune@verizonmedia.com>
2021-04-27 15:16:31 +0200

    no travis

--- travis/compile-and-test-docker.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-
-set -e
-
-docker run --rm -v $(pwd):/source -w /source --entrypoint /source/travis/compile-and-test.sh vespaengine/vespa-pipeline
