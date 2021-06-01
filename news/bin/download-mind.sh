9f833cc97571ee22fde92d5802dde361bc10c269
Kristian Aune <kkraune@users.noreply.github.com>
2021-06-01 14:27:41 +0200

    use curl
    - wget not default installed on macs

--- news/bin/download-mind.sh
+++ news/bin/download-mind.sh
@@ -10,18 +10,18 @@ mkdir -p $DEV_DIR
 
 if [ $DATASET = "demo" ]
 then
-  wget -O $DIR/train.zip -nd https://recodatasets.blob.core.windows.net/newsrec/MINDdemo_train.zip
-  wget -O $DIR/dev.zip   -nd https://recodatasets.blob.core.windows.net/newsrec/MINDdemo_dev.zip
+  curl -L -o $DIR/train.zip https://recodatasets.blob.core.windows.net/newsrec/MINDdemo_train.zip
+  curl -L -o $DIR/dev.zip https://recodatasets.blob.core.windows.net/newsrec/MINDdemo_dev.zip
 
 elif [ $DATASET = "small" ]
 then
-  wget -O $DIR/train.zip -nd https://mind201910small.blob.core.windows.net/release/MINDsmall_train.zip
-  wget -O $DIR/dev.zip   -nd https://mind201910small.blob.core.windows.net/release/MINDsmall_dev.zip
+  curl -L -o $DIR/train.zip -nd https://mind201910small.blob.core.windows.net/release/MINDsmall_train.zip
+  curl -L -o $DIR/dev.zip   -nd https://mind201910small.blob.core.windows.net/release/MINDsmall_dev.zip
 
 elif [ $DATASET = "large" ]
 then
-  wget -O $DIR/train.zip -nd https://mind201910small.blob.core.windows.net/release/MINDlarge_train.zip
-  wget -O $DIR/dev.zip   -nd https://mind201910small.blob.core.windows.net/release/MINDlarge_dev.zip
+  curl -L -o $DIR/train.zip -nd https://mind201910small.blob.core.windows.net/release/MINDlarge_train.zip
+  curl -L -o $DIR/dev.zip   -nd https://mind201910small.blob.core.windows.net/release/MINDlarge_dev.zip
 
 else
   echo "No dataset specified. Use demo|small|large."
