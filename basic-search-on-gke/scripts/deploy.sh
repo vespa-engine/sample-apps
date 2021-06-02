f6dc2f2abc57923f4f397e8a9c0b048b3e0905e7
Harald Musum <musum@verizonmedia.com>
2021-06-02 21:33:09 +0200

    searchdefinitions -> schemas

--- basic-search-on-gke/scripts/deploy.sh
+++ basic-search-on-gke/scripts/deploy.sh
@@ -7,6 +7,6 @@ kubectl apply -f deployments/configmap.yml -f deployments/master.yml -f deployme
 while [[ $(kubectl get pods -l app=vespa -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}' | sort -u) != "True" ]]; do echo "waiting for pod" && sleep 10; done
 kubectl cp src/main/application/hosts.xml vespa-0:/workspace
 kubectl cp src/main/application/services.xml vespa-0:/workspace
-kubectl cp src/main/application/searchdefinitions vespa-0:/workspace
+kubectl cp src/main/application/schemas vespa-0:/workspace
 kubectl exec vespa-0 -- bash -c '/opt/vespa/bin/vespa-deploy prepare /workspace && /opt/vespa/bin/vespa-deploy activate'
 kubectl exec vespa-0 -- bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:8080/ApplicationStatus)" != "200" ]]; do sleep 10; done'
