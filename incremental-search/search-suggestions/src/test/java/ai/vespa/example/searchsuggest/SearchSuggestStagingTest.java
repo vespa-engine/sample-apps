3abb1e3960d35fdd32429d7a71cecea36c10feff
Ole-Magnus Vian Norum <olemagnusvian.norum@verizonmedia.com>
2021-06-22 08:16:40 +0200

    Add search-suggestion deployment and tests

---
+++ incremental-search/search-suggestions/src/test/java/ai/vespa/example/searchsuggest/SearchSuggestStagingTest.java
@@ -0,0 +1,13 @@
+package ai.vespa.example.searchsuggest;
+
+import ai.vespa.hosted.cd.StagingTest;
+import org.junit.jupiter.api.Test;
+
+@StagingTest
+public class SearchSuggestStagingTest {
+
+    @Test
+    public void testSearchAndFeeding() throws Exception{
+
+    }
+}
