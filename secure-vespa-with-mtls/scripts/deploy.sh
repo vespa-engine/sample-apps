6a9350fc04d883a3c81c6172807267b201a42a7f
Bjørn Christian Seime <bjorncs@verizonmedia.com>
2021-08-19 15:52:24 +0200

    Use prepareandactivate

--- secure-vespa-with-mtls/scripts/deploy.sh
+++ secure-vespa-with-mtls/scripts/deploy.sh
@@ -6,7 +6,4 @@ set -x
 readonly MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $MYDIR
 
-readonly SID=$(tar -C ../src/main/application -cf - . | gzip | $MYDIR/vespa-curl.sh -s -L --header "Content-Type: application/x-gzip" --data-binary @- "https://localhost:19071/application/v2/tenant/default/session" | python -c "import sys,json; print(json.load(sys.stdin)['session-id']);")
-$MYDIR/vespa-curl.sh -s -L -X PUT "https://localhost:19071/application/v2/tenant/default/session/$SID/prepared" | python -mjson.tool
-$MYDIR/vespa-curl.sh -s -L -X PUT "https://localhost:19071/application/v2/tenant/default/session/$SID/active" | python -mjson.tool
-
+tar -C ../src/main/application -cf - . | gzip | $MYDIR/vespa-curl.sh -s -L --header "Content-Type: application/x-gzip" --data-binary @- "https://localhost:19071/application/v2/tenant/default/prepareandactivate" | python -mjson.tool
