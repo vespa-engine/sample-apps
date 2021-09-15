a79d4cb32ff2fe01ccb97febbbf43cf92ed61cb2
Jon Marius Venstad <venstad@gmail.com>
2021-09-15 11:41:13 +0200

    Add variants for joining with OR-ed ranges, or in container

--- vespa-cloud/joins/src/main/java/ai/vespa/example/joins/JoinSearcher.java
+++ vespa-cloud/joins/src/main/java/ai/vespa/example/joins/JoinSearcher.java
@@ -1,9 +1,12 @@
 // Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package ai.vespa.example.joins;
 
-import com.yahoo.prelude.query.IntItem;
+import ai.vespa.example.joins.Intersector.Intersection;
+import ai.vespa.example.joins.Intersector.Interval;
+import com.yahoo.prelude.query.AndItem;
+import com.yahoo.prelude.query.Item;
 import com.yahoo.prelude.query.Limit;
-import com.yahoo.prelude.query.NotItem;
+import com.yahoo.prelude.query.OrItem;
 import com.yahoo.prelude.query.RangeItem;
 import com.yahoo.prelude.query.WordItem;
 import com.yahoo.search.Query;
@@ -13,39 +16,45 @@ import com.yahoo.search.result.Hit;
 import com.yahoo.search.searchchain.Execution;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public class JoinSearcher extends Searcher {
 
     @Override
     public Result search(Query query, Execution execution) {
-        Optional<JoinSpec> joinSpec = JoinSpec.from(query);
-        if (joinSpec.isEmpty()) return execution.search(query); // No join spec so skip this searcher
+        Optional<JoinSpec> spec = JoinSpec.from(query);
+        if (spec.isEmpty()) return execution.search(query); // No join spec so skip this searcher
 
-        Query inputQuery = query.clone();
-
-        Result aResult = execution.search(joinSpec.get().addATerms(query));
+        Result aResult = execution.search(spec.get().addATerms(query.clone()));
         execution.fillAttributes(aResult);
 
-        return join(inputQuery, aResult, joinSpec.get(), execution);
+        switch (spec.get().variant) {
+            case queryPerHit: return joinWithQueryPerHit(query.clone(), aResult, spec.get(), execution);
+            case rangeItemPerHit: return joinWithRangeItemPerHit(query.clone(), aResult, spec.get(), execution);
+            case containerIntersect: return joinInContainer(query.clone(), aResult, spec.get(), execution);
+            default: throw new IllegalStateException("Unexpected join spec variant: " + spec.get().variant);
+        }
     }
 
-    private Result join(Query query, Result aResult, JoinSpec spec, Execution execution) {
+    private Result joinWithQueryPerHit(Query query, Result aResult, JoinSpec spec, Execution execution) {
         Result result = new Result(query);
         for (Hit hit : aResult.hits()) {
             if (hit.isAuxiliary()) {
                 result.hits().add(hit);
             }
             else {
-                for (Hit joinedHit : joinWith(hit, query.clone(), spec, execution))
+                for (Hit joinedHit : joinWithQuery(hit, query.clone(), spec, execution))
                     result.hits().add(joinedHit);
             }
         }
         return result;
     }
 
-    private List<Hit> joinWith(Hit aHit, Query query, JoinSpec spec, Execution execution) {
+    private List<Hit> joinWithQuery(Hit aHit, Query query, JoinSpec spec, Execution execution) {
         spec.addBTerms(query);
         query.getModel().getQueryTree().and(new WordItem(aHit.getField("id").toString(), "id"));
         spec.addInterval((long)aHit.getField("start"), (long)aHit.getField("end"), query);
@@ -62,7 +71,105 @@ public class JoinSearcher extends Searcher {
         return joinedHits;
     }
 
+    private Result joinWithRangeItemPerHit(Query query, Result aResult, JoinSpec spec, Execution execution) {
+        OrItem rangesRoot = new OrItem();
+        for (Hit hit : aResult.hits()) {
+            if ( ! hit.isAuxiliary()) {
+                rangesRoot.addItem(toRangeItem(hit));
+            }
+        }
+        spec.addBTerms(query);
+        query.getModel().getQueryTree().and(rangesRoot);
+
+        Result result = execution.search(query);
+        execution.fillAttributes(result);
+
+        for (Hit hit : aResult.hits()) {
+            if (hit.isAuxiliary()) {
+                result.hits().add(hit);
+            }
+        }
+        return result;
+    }
+
+    private Item toRangeItem(Hit aHit) {
+        AndItem rangeItem = new AndItem();
+        rangeItem.addItem(new WordItem(aHit.getField("id").toString(), "id"));
+        rangeItem.addItem(new RangeItem(Limit.NEGATIVE_INFINITY, new Limit((long) aHit.getField("end"), true), "start"));
+        rangeItem.addItem(new RangeItem(new Limit((long) aHit.getField("start"), true), Limit.POSITIVE_INFINITY, "end"));
+        return rangeItem;
+    }
+
+    private Result joinInContainer(Query query, Result aResult, JoinSpec spec, Execution execution) {
+        Result result = new Result(query);
+        Map<String, List<HitInterval>> hitsById = new LinkedHashMap<>();
+        for (Hit hit : aResult.hits()) {
+            if (hit.isAuxiliary()) {
+                result.hits().add(hit);
+            }
+            else {
+                hitsById.computeIfAbsent(hit.getField("id").toString(), __ -> new ArrayList<>())
+                        .add(new HitInterval(hit));
+            }
+        }
+
+        spec.addBTerms(query);
+        hitsById.forEach((id, intervals) -> {
+            for (Hit joinedHit : joinInContainer(query.clone(), id, intervals, execution)) {
+                result.hits().add(joinedHit);
+            }
+        });
+        return result;
+    }
+
+    private List<Hit> joinInContainer(Query query, String id, List<HitInterval> aIntervals, Execution execution) {
+        query.getModel().getQueryTree().and(new WordItem(id, "id"));
+        Result bResult = execution.search(query);
+        execution.fillAttributes(bResult);
+
+        List<HitInterval> bIntervals = new ArrayList<>();
+        for (Hit bHit : bResult.hits()) {
+            if ( ! bHit.isAuxiliary()) {
+                bIntervals.add(new HitInterval(bHit));
+            }
+        }
+
+        aIntervals.sort(Comparator.comparingLong(Interval::start));
+        bIntervals.sort(Comparator.comparingLong(Interval::start));
+
+        List<Hit> joinedResults = new ArrayList<>();
+        for (Intersection<HitInterval, HitInterval> intersection : Intersector.intersectX(aIntervals, bIntervals)) {
+            Hit joinedHit = intersection.first().hit.clone();
+            joinedHit.setField("tagid", intersection.second().hit.getField("tagid"));
+            joinedHit.setField("tagStart", intersection.second().start());
+            joinedHit.setField("tagEnd", intersection.second().end());
+            joinedResults.add(joinedHit);
+        }
+        return joinedResults;
+    }
+
     static class JoinSpec {
+
+        enum Variant {
+            /** Do a sub-query per hit for query a, based on query b. */
+            queryPerHit,
+            /** Combine all hits from query a to an additional filter for query b. */
+            rangeItemPerHit,
+            /** Get all hits for queries a and b, and combine the results in a searcher. */
+            containerIntersect;
+
+            static Variant fromString(String value) {
+                if (value == null) return queryPerHit;
+                switch (value) {
+                    case "queryPerHit": return queryPerHit;
+                    case "rangeItemPerHit": return rangeItemPerHit;
+                    case "containerIntersect": return containerIntersect;
+                    default: throw new IllegalArgumentException("Must specify variant= one of " + List.of(values()));
+                }
+            }
+        }
+
+        final Variant variant;
 
         final String aType;
         final String aField;
@@ -73,8 +180,9 @@ public class JoinSearcher extends Searcher {
         final String bField;
         final String bValue;
 
-        public JoinSpec(String aType, String aField, String aValue, long aStart, long aEnd,
+        public JoinSpec(Variant variant, String aType, String aField, String aValue, long aStart, long aEnd,
                         String bType, String bField, String bValue) {
+            this.variant = variant;
             this.aType = aType;
             this.aField = aField;
             this.aValue = aValue;
@@ -104,6 +212,7 @@ public class JoinSearcher extends Searcher {
         }
 
         static Optional<JoinSpec> from(Query query) {
+            Variant variant = Variant.fromString(query.properties().getString("variant"));
             String aType = query.properties().getString("a.type");
             String aField = query.properties().getString("a.field");
             String aValue = query.properties().getString("a.value");
@@ -116,7 +225,28 @@ public class JoinSearcher extends Searcher {
             if (aType == null || aField == null || aValue == null || aStart == null || aEnd == null ||
                 bType == null || bField == null || bValue == null)
                 return Optional.empty();
-            return Optional.of(new JoinSpec(aType, aField, aValue, aStart, aEnd, bType, bField, bValue));
+            return Optional.of(new JoinSpec(variant, aType, aField, aValue, aStart, aEnd, bType, bField, bValue));
+        }
+
+    }
+
+
+    static class HitInterval implements Interval {
+
+        private final Hit hit;
+
+        HitInterval(Hit hit) {
+            this.hit = hit;
+        }
+
+        @Override
+        public long start() {
+            return (long) hit.getField("start");
+        }
+
+        @Override
+        public long end() {
+            return (long) hit.getField("end");
         }
 
     }
