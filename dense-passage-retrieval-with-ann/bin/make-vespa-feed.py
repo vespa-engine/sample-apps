d81f22c24ed3df693648fc8fc6714bdba41212f4
Jo Kristian Bergum <bergum@oath.com>
2021-10-20 15:10:11 +0200

    Adjust script after load_passages went missing

--- dense-passage-retrieval-with-ann/bin/make-vespa-feed.py
+++ dense-passage-retrieval-with-ann/bin/make-vespa-feed.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python3
-
 # Copyright Verizon Media. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
+import csv
 import sys
 import numpy as np
 import json
 
 sys.path.append("DPR")
 
-from dense_retriever import iterate_encoded_files, load_passages
+from dense_retriever import iterate_encoded_files
+
+def load_passages(file):
+  docs = dict()
+  with open(file) as csvfile:
+    reader = csv.DictReader(csvfile, delimiter='\t', quotechar='"')
+    for row in reader:
+      id = row['id']
+      text = row['text']
+      title = row['title']
+      docs[id] = (text,title)
+  return docs
 
 docs = load_passages(sys.argv[1])
 vector_files = sys.argv[2:]
@@ -39,3 +50,4 @@ for i, item in enumerate(iterate_encoded_files(vector_files)):
   }
   json.dump(doc,sys.stdout)
   sys.stdout.write('\n')
+
