53af21538e329aab6ed1d95f9e96735fdbeea900
tmartins <thigm85@gmail.com>
2021-12-06 10:51:29 -0300

    retrieve images and add timing

--- text-image-search/src/python/app.py
+++ text-image-search/src/python/app.py
@@ -1,21 +1,22 @@
 import os
 import clip
+import time
 import streamlit as st
 from math import floor
 from PIL import Image
 
 from vespa.application import Vespa
 from vespa.query import QueryModel, ANN, QueryRankingFeature, RankProfile as Ranking
-from embedding import translate_model_names_to_valid_vespa_field_names, TextProcessor
+from embedding import (
+    translate_model_names_to_valid_vespa_field_names,
+    TextProcessor,
+    decode_string_to_media,
+)
 
 st.set_page_config(layout="wide")
 
 VESPA_URL = os.environ.get("VESPA_ENDPOINT", "http://localhost:8080")
 VESPA_CERT_PATH = os.environ.get("VESPA_CERT_PATH", None)
-try:
-    IMG_FOLDER = os.environ["IMG_DIR"]
-except KeyError:
-    raise ValueError("Set IMG_DIR env variable")
 
 app = Vespa(
     url=VESPA_URL,
@@ -59,30 +60,43 @@ def vespa_query(query, clip_model_name):
                 QueryRankingFeature(name=text_vector_name, mapping=text_processor.embed)
             ],
         ),
+        **{"presentation.timing": "true"}
     )
-    return [hit["fields"]["image_file_name"] for hit in result.hits]
+    return [hit["fields"]["encoded_image"] for hit in result.hits], result.json[
+        "timing"
+    ]
 
 
 clip_model_name = st.sidebar.selectbox(
     "Select CLIP model", get_available_clip_model_names()
 )
 
-out1, col1, out2 = st.columns([3,1,3])
+out1, col1, out2 = st.columns([3, 1, 3])
 col1.image("https://docs.vespa.ai/assets/vespa-logo-color.png", width=100)
 query_input = st.text_input(label="", value="a man surfing", key="query_input")
 
-image_file_names = vespa_query(query=query_input, clip_model_name=clip_model_name)
-
-number_rows = floor(len(image_file_names)/3)
-remainder = len(image_file_names) % 3
+start = time.time()
+images, timing = vespa_query(query=query_input, clip_model_name=clip_model_name)
+placeholder = st.empty()
+number_rows = floor(len(images) / 3)
+remainder = len(images) % 3
 if number_rows > 0:
     for i in range(number_rows):
         col1, col2, col3 = st.columns(3)
-        col1.image(get_image(image_file_names[3*i], image_dir=IMG_FOLDER))
-        col2.image(get_image(image_file_names[3*i + 1], image_dir=IMG_FOLDER))
-        col3.image(get_image(image_file_names[3*i + 2], image_dir=IMG_FOLDER))
+        col1.image(decode_string_to_media(images[3 * i]))
+        col2.image(decode_string_to_media(images[3 * i + 1]))
+        col3.image(decode_string_to_media(images[3 * i + 2]))
 if remainder > 0:
     cols = st.columns(3)
     for i in range(remainder):
-        cols[i].image(get_image(image_file_names[3*number_rows+i], image_dir=IMG_FOLDER))
+        cols[i].image(decode_string_to_media(images[3 * number_rows + i]))
+total_timing = time.time() - start
+vespa_search_time = round(timing["searchtime"], 2)
+total_time = round(total_timing, 2)
+other_time = round(total_time - vespa_search_time, 2)
+placeholder.write(
+    "**Vespa search time: {}s**. Network related time: {}s. Total time: {}s".format(
+        vespa_search_time, other_time, total_time
+    )
+)
 
