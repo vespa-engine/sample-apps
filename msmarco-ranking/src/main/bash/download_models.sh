254e823dc22c14868dce23ecb4f61968b65a81b3
Kristian Aune <kraune@verizonmedia.com>
2022-02-14 15:22:41 +0100

    Simplify, make work in new image without pushd/popd

--- msmarco-ranking/src/main/bash/download_models.sh
+++ msmarco-ranking/src/main/bash/download_models.sh
@@ -5,16 +5,15 @@ DIR="$1"
 echo "[INFO] Downloading models into $DIR"
 
 mkdir -p $DIR
-pushd $DIR
-    echo "Downloading https://data.vespa.oath.cloud/onnx_models/ms-marco-MiniLM-L-6-v2-quantized.onnx"
-    curl -L -o msmarco_v2.onnx \
-    https://data.vespa.oath.cloud/onnx_models/ms-marco-MiniLM-L-6-v2-quantized.onnx 
 
-    echo "Downloading https://data.vespa.oath.cloud/onnx_models/sentence-msmarco-MiniLM-L-6-v3-quantized.onnx"
-    curl -L -o dense_encoder.onnx \
-    https://data.vespa.oath.cloud/onnx_models/sentence-msmarco-MiniLM-L-6-v3-quantized.onnx
+echo "Downloading https://data.vespa.oath.cloud/onnx_models/ms-marco-MiniLM-L-6-v2-quantized.onnx"
+curl -L -o $DIR/msmarco_v2.onnx \
+https://data.vespa.oath.cloud/onnx_models/ms-marco-MiniLM-L-6-v2-quantized.onnx
 
-    echo "Downloading https://data.vespa.oath.cloud/onnx_models/vespa-colMiniLM-L-6-quantized.onnx"
-    curl -L -o colbert_encoder.onnx \
-    https://data.vespa.oath.cloud/onnx_models/vespa-colMiniLM-L-6-quantized.onnx
-popd
+echo "Downloading https://data.vespa.oath.cloud/onnx_models/sentence-msmarco-MiniLM-L-6-v3-quantized.onnx"
+curl -L -o $DIR/dense_encoder.onnx \
+https://data.vespa.oath.cloud/onnx_models/sentence-msmarco-MiniLM-L-6-v3-quantized.onnx
+
+echo "Downloading https://data.vespa.oath.cloud/onnx_models/vespa-colMiniLM-L-6-quantized.onnx"
+curl -L -o $DIR/colbert_encoder.onnx \
+https://data.vespa.oath.cloud/onnx_models/vespa-colMiniLM-L-6-quantized.onnx
