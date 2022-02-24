cabd7a6dd4487b12359344b9612ee886cc8adb54
Kristian Aune <kraune@verizonmedia.com>
2022-02-24 15:26:45 +0100

    First cut link checking in app package files

---
+++ _plugins-linkcheck/vespa_xml_links_generator.rb
@@ -0,0 +1,45 @@
+# Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+require 'nokogiri'
+
+module Jekyll
+
+    # This generator creates links-to-check.html with links extracted from comments in named XML files
+    # This file can be moved into the _site directory for link check subsequently
+
+    class VespaXMLLinksGenerator < Jekyll::Generator
+        priority :lowest
+
+        def generate(site)
+            all_links = []
+            site.pages.each do |page|
+                if ["services.xml", "hosts.xml", "deployment.xml"].include?(page.name)
+                    extract_links(page).each do |link|
+                        all_links.push(link)
+                    end
+                end
+            end
+            link_file = "<!DOCTYPE html><html><head><title>links</title></head><body>\n"
+            all_links.uniq.each do |link|
+                link_file += "<a href='" + link + "'>" + link + "</a>\n"
+            end
+            link_file += "</body></html>\n"
+            File.open("links-to-check.html", "w") { |f| f.write(link_file) }
+        end
+
+        def extract_links(page)
+            doc = Nokogiri::HTML(page.content)
+            all_urls = []
+            comments = doc.xpath("//comment()")
+            comments.each do |comment|
+                urls = comment.to_s.split(/\s+/).find_all { |u| u =~ /^https?:/ }
+                urls.each do |url|
+                    all_urls.push(url)
+                end
+            end
+            return all_urls
+        end
+
+    end
+
+end
