f2eadb4b320e94caf6a8637193eceb20a18d6797
Jo Kristian Bergum <bergum@oath.com>
2022-03-04 10:49:48 +0100

    add preprocess and fix legacy copyrights

--- use-case-shopping/convert_reviews.py
+++ use-case-shopping/convert_reviews.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2019 Oath Inc. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+# Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
 import sys
 import time
