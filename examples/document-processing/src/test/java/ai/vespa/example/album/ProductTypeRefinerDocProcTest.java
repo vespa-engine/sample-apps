9fcf6182a4ef959465e91bc01ddebd4c9fdd0721
gjoranv <gv@verizonmedia.com>
2022-03-08 23:55:58 +0100

    Use Application framework to avoid access to deprecated types.

--- examples/document-processing/src/test/java/ai/vespa/example/album/ProductTypeRefinerDocProcTest.java
+++ examples/document-processing/src/test/java/ai/vespa/example/album/ProductTypeRefinerDocProcTest.java
@@ -1,18 +1,17 @@
 // Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package ai.vespa.example.album;
 
-import com.yahoo.docproc.CallStack;
-import com.yahoo.docproc.DocprocService;
-import com.yahoo.docproc.DocumentProcessor;
+import com.yahoo.application.Application;
+import com.yahoo.application.container.JDisc;
+import com.yahoo.component.ComponentSpecification;
 import com.yahoo.docproc.Processing;
-import com.yahoo.docproc.jdisc.metric.NullMetric;
-import com.yahoo.document.DataType;
 import com.yahoo.document.Document;
-import com.yahoo.document.DocumentOperation;
 import com.yahoo.document.DocumentPut;
-import com.yahoo.document.DocumentType;
 import com.yahoo.document.datatypes.StringFieldValue;
 import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static ai.vespa.example.album.ProductTypeRefinerDocProc.MUSIC_DOCUMENT_TYPE;
 import static ai.vespa.example.album.ProductTypeRefinerDocProc.PRODUCT_TYPE_FIELD_NAME;
@@ -20,40 +19,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ProductTypeRefinerDocProcTest {
 
-    private static DocprocService setupDocprocService(DocumentProcessor processor) {
-        CallStack stack = new CallStack("default", new NullMetric());
-        stack.addLast(processor);
-        DocprocService service = new DocprocService("default");
-        service.setCallStack(stack);
-        service.setInService(true);
-        return service;
-    }
-
-    private static Processing getProcessing(DocumentOperation... operations) {
-        Processing processing = new Processing();
-        for (DocumentOperation op : operations) {
-            processing.addDocumentOperation(op);
-        }
-        return processing;
-    }
-
-    private static DocumentType createMusicType() {
-        DocumentType type = new DocumentType(MUSIC_DOCUMENT_TYPE);
-        type.addField(PRODUCT_TYPE_FIELD_NAME, DataType.STRING);
-        return type;
-    }
-
     @Test
-    public void testRefinement() {
-        Document doc = new Document(createMusicType(), "id:mynamespace:music::a-head-full-of-dreams");
-        doc.setFieldValue(PRODUCT_TYPE_FIELD_NAME,
-                new StringFieldValue("Media > Music & Sound Recordings > Music Cassette Tapes"));
+    public void testRefinement () throws Exception {
+        Application app = Application.fromBuilder(
+                new Application.Builder()
+                        .documentType(MUSIC_DOCUMENT_TYPE, Files.readString(Path.of("src/main/application/schemas/" + MUSIC_DOCUMENT_TYPE + ".sd")))
+                        .container("myContainer", new Application.Builder.Container()
+                                .documentProcessor("myChain", ProductTypeRefinerDocProc.class)
+                        ));
+        JDisc container = app.getJDisc("myContainer");
 
-        Processing p = getProcessing(new DocumentPut(doc));
-        DocprocService service = setupDocprocService(new ProductTypeRefinerDocProc());
-        service.getExecutor().process(p);
+        Processing processing = new Processing();
+        Document doc = new Document(container.documentProcessing().getDocumentTypes().get(MUSIC_DOCUMENT_TYPE),
+                                    "id:mynamespace:" + MUSIC_DOCUMENT_TYPE + "::a-head-full-of-dreams");
+        doc.setFieldValue(PRODUCT_TYPE_FIELD_NAME,
+                          new StringFieldValue("Media > Music & Sound Recordings > Music Cassette Tapes"));
+
+        processing.addDocumentOperation(new DocumentPut(doc));
+        container.documentProcessing().processOnce(ComponentSpecification.fromString("myChain"), processing);
 
         assertEquals("Media | Music & Sound Recordings | Music Cassette Tapes",
-                doc.getFieldValue(PRODUCT_TYPE_FIELD_NAME).toString());
+                     doc.getFieldValue(PRODUCT_TYPE_FIELD_NAME).toString());
     }
 }
