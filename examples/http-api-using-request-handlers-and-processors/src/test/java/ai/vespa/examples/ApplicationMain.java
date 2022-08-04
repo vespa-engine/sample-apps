adb78effa73140c6e17a04ba34d3a223dde04891
Kristian Aune <kraune@verizonmedia.com>
2022-08-04 13:04:58 +0200

    Use ai.vespa.examples

--- examples/http-api-using-request-handlers-and-processors/src/test/java/ai/vespa/examples/ApplicationMain.java
+++ examples/http-api-using-request-handlers-and-processors/src/test/java/ai/vespa/examples/ApplicationMain.java
@@ -1,5 +1,5 @@
 // Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-package com.mydomain.examples;
+package ai.vespa.examples;
 
 import com.yahoo.application.Networking;
 import org.junit.jupiter.api.Test;
