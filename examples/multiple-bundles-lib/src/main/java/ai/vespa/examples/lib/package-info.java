e0eb8ced96c9a7f413ceebcb0d62aa57db430320
Kristian Aune <kraune@verizonmedia.com>
2022-08-04 12:45:19 +0200

    Use ai.vespa.examples

--- examples/multiple-bundles-lib/src/main/java/ai/vespa/examples/lib/package-info.java
+++ examples/multiple-bundles-lib/src/main/java/ai/vespa/examples/lib/package-info.java
@@ -1,5 +1,5 @@
 // Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 @ExportPackage
-package com.mydomain.lib;
+package ai.vespa.examples.lib;
 
 import com.yahoo.osgi.annotation.ExportPackage;
