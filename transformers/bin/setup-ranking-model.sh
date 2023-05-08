06259b4c4c2f6db4b8925e3a6b409da8cabb3ff4
Kristian Aune <kkraune@users.noreply.github.com>
2023-05-08 08:16:53 +0200

    Kkraune/bugfix (#1192)
    * correct script location
    
    * onnx model location
    
    * local endpoint already assumed

--- transformers/bin/setup-ranking-model.sh
+++ transformers/bin/setup-ranking-model.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-FILE="src/main/application/files/ranking_model.onnx"
+FILE="application/files/ranking_model.onnx"
 
 echo "Setting up ranking model..."
-./setup-model.py $FILE
+./bin/setup-model.py $FILE
 echo "Done setting up model..."
