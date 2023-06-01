6dbb027e934c4d950da5bc37fcb4e47223df9cff
Kristian Aune <kraune@verizonmedia.com>
2023-06-01 09:26:14 +0200

    run as user 1000, scripts more readable

--- examples/operations/basic-search-on-gke/scripts/deploy.sh
+++ examples/operations/basic-search-on-gke/scripts/deploy.sh
@@ -3,10 +3,31 @@
 set -euo pipefail
 set -x
 
-kubectl apply -f deployments/configmap.yml -f deployments/master.yml -f deployments/container.yml -f deployments/content.yml -f deployments/headless.yml -f deployments/service.yml
-while [[ $(kubectl get pods -l app=vespa -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}' | sort -u) != "True" ]]; do echo "waiting for pod" && sleep 10; done
-kubectl cp hosts.xml vespa-0:/workspace
-kubectl cp services.xml vespa-0:/workspace
-kubectl cp schemas vespa-0:/workspace
-kubectl exec vespa-0 -- bash -c '/opt/vespa/bin/vespa-deploy prepare /workspace && /opt/vespa/bin/vespa-deploy activate'
-kubectl exec vespa-0 -- bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:8080/ApplicationStatus)" != "200" ]]; do sleep 10; done'
+kubectl apply \
+  -f deployments/configmap.yml \
+  -f deployments/master.yml \
+  -f deployments/container.yml \
+  -f deployments/content.yml \
+  -f deployments/headless.yml \
+  -f deployments/service.yml
+
+while [[ $(kubectl get pods -l app=vespa -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}' | sort -u) != "True" ]]
+do
+  echo "waiting for pods..." && sleep 10
+done
+
+CFG_SRV_HOST="$(kubectl get service/vespa -o jsonpath='{.status.loadBalancer.ingress[*].ip}')"
+CFG_SRV_PORT="$(kubectl get service/vespa -o jsonpath='{.spec.ports[?(@.name=="config")].port}')"
+CFG_SRV_ENDPOINT="$CFG_SRV_HOST:$CFG_SRV_PORT"
+
+zip -r - . \
+  -x .gitignore "deployments/*" "ext/*" "scripts/*" "templates/*" README.md "config/*" | \
+  curl --header Content-Type:application/zip \
+  --data-binary @- \
+  "http://$CFG_SRV_ENDPOINT/application/v2/tenant/default/prepareandactivate"
+
+kubectl exec vespa-0 -- bash -c \
+  'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:8080/ApplicationStatus)" != "200" ]]
+  do
+    sleep 10
+  done'
