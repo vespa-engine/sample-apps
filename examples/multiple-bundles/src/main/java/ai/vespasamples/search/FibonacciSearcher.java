c64ad65d7f7ee9892ec1f3bbff01a88131927f74
gjoranv <gv@yahooinc.com>
2023-06-05 16:52:13 +0200

    Set 'failOnWarnings' true for sample apps without parent.

--- examples/multiple-bundles/src/main/java/ai/vespasamples/search/FibonacciSearcher.java
+++ examples/multiple-bundles/src/main/java/ai/vespasamples/search/FibonacciSearcher.java
@@ -1,7 +1,7 @@
 // Copyright Yahoo. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-package ai.vespa.examples.search;
+package ai.vespasamples.search;
 
-import ai.vespa.examples.lib.FibonacciProducer;
+import ai.vespasamples.lib.FibonacciProducer;
 import com.yahoo.search.Query;
 import com.yahoo.search.Result;
 import com.yahoo.search.Searcher;
