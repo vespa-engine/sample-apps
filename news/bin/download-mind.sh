0d05a6c3b96e854ef95e77ff5cda2ec599ff5d15
Arne Juul <arnej@yahooinc.com>
2023-06-06 11:27:54 +0000

    remove "-nd" option intended for wget

--- news/bin/download-mind.sh
+++ news/bin/download-mind.sh
@@ -8,20 +8,20 @@ DEV_DIR="$DIR/dev"
 mkdir -p $TRAIN_DIR
 mkdir -p $DEV_DIR
 
-if [ $DATASET = "demo" ]
+if [ "$DATASET" = "demo" ]
 then
-  curl -L -o $DIR/train.zip https://recodatasets.z20.web.core.windows.net/newsrec/MINDdemo_train.zip
-  curl -L -o $DIR/dev.zip https://recodatasets.z20.web.core.windows.net/newsrec/MINDdemo_dev.zip
+  curl -L -o $DIR/train.zip  https://recodatasets.z20.web.core.windows.net/newsrec/MINDdemo_train.zip
+  curl -L -o $DIR/dev.zip    https://recodatasets.z20.web.core.windows.net/newsrec/MINDdemo_dev.zip
 
-elif [ $DATASET = "small" ]
+elif [ "$DATASET" = "small" ]
 then
-  curl -L -o $DIR/train.zip -nd https://mind201910small.blob.core.windows.net/release/MINDsmall_train.zip
-  curl -L -o $DIR/dev.zip   -nd https://mind201910small.blob.core.windows.net/release/MINDsmall_dev.zip
+  curl -L -o $DIR/train.zip  https://mind201910small.blob.core.windows.net/release/MINDsmall_train.zip
+  curl -L -o $DIR/dev.zip    https://mind201910small.blob.core.windows.net/release/MINDsmall_dev.zip
 
-elif [ $DATASET = "large" ]
+elif [ "$DATASET" = "large" ]
 then
-  curl -L -o $DIR/train.zip -nd https://mind201910small.blob.core.windows.net/release/MINDlarge_train.zip
-  curl -L -o $DIR/dev.zip   -nd https://mind201910small.blob.core.windows.net/release/MINDlarge_dev.zip
+  curl -L -o $DIR/train.zip  https://mind201910small.blob.core.windows.net/release/MINDlarge_train.zip
+  curl -L -o $DIR/dev.zip    https://mind201910small.blob.core.windows.net/release/MINDlarge_dev.zip
 
 else
   echo "No dataset specified. Use demo|small|large."
