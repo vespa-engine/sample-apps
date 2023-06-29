f45c0a0a60d16f5c1f481c82cf1ce1108fe99bfc
Kristian Aune <kraune@verizonmedia.com>
2023-06-29 09:00:11 +0200

    Remove wget from guides

--- transformers/bin/download-msmarco.sh
+++ transformers/bin/download-msmarco.sh
@@ -4,10 +4,10 @@ DIR="msmarco/download"
 
 mkdir -p $DIR
 
-wget -P $DIR -nd https://msmarco.blob.core.windows.net/msmarcoranking/msmarco-doctrain-queries.tsv.gz
-wget -P $DIR -nd https://msmarco.blob.core.windows.net/msmarcoranking/msmarco-doctrain-qrels.tsv.gz
-wget -P $DIR -nd https://msmarco.blob.core.windows.net/msmarcoranking/msmarco-docs-lookup.tsv.gz
-wget -P $DIR -nd https://msmarco.blob.core.windows.net/msmarcoranking/msmarco-docs.tsv.gz
+curl -L -o $DIR/msmarco-doctrain-queries.tsv.gz https://msmarco.blob.core.windows.net/msmarcoranking/msmarco-doctrain-queries.tsv.gz
+curl -L -o $DIR/msmarco-doctrain-qrels.tsv.gz https://msmarco.blob.core.windows.net/msmarcoranking/msmarco-doctrain-qrels.tsv.gz
+curl -L -o $DIR/msmarco-docs-lookup.tsv.gz https://msmarco.blob.core.windows.net/msmarcoranking/msmarco-docs-lookup.tsv.gz
+curl -L -o $DIR/msmarco-docs.tsv.gz https://msmarco.blob.core.windows.net/msmarcoranking/msmarco-docs.tsv.gz
 
 gunzip $DIR/msmarco-docs.tsv.gz
 
