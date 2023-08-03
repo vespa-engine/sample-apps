9442d626d15a224075ad57983e5901f7f27a25de
Torstein Egge <torstein.egge@yahooinc.com>
2023-08-03 12:58:32 +0200

    Remove scripts that were only used for a very specific use case

--- examples/embedder-auto-training-evaluation/vm-scripts/local-send-app-to-vm.sh
+++
@@ -1,4 +0,0 @@
-
-# i-046aacad7834f9514 == Torstein's vm (simple-ec2-instance)
-scp -O -A -o ProxyJump=35.170.131.31:2222 ../cloudbeir/feed.jsonl i-046aacad7834f9514:
-scp -r -O -A -o ProxyJump=35.170.131.31:2222 ../application-package i-046aacad7834f9514:
