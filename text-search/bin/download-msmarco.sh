d2d85009d0fca4cdc24cc35b07fd8f286ea80f27
Paul Frybarger <pfrybar@frymail.net>
2024-03-08 14:36:13 +0000

    change msmarco dataset location (#1403)

--- text-search/bin/download-msmarco.sh
+++ text-search/bin/download-msmarco.sh
@@ -4,9 +4,9 @@ DIR="msmarco/download"
 
 mkdir -p $DIR
 
-curl -L -o $DIR/msmarco-doctrain-queries.tsv.gz https://msmarco.blob.core.windows.net/msmarcoranking/msmarco-doctrain-queries.tsv.gz
-curl -L -o $DIR/msmarco-doctrain-qrels.tsv.gz https://msmarco.blob.core.windows.net/msmarcoranking/msmarco-doctrain-qrels.tsv.gz
-curl -L -o $DIR/msmarco-docs-lookup.tsv.gz https://msmarco.blob.core.windows.net/msmarcoranking/msmarco-docs-lookup.tsv.gz
-curl -L -o $DIR/msmarco-docs.tsv.gz https://msmarco.blob.core.windows.net/msmarcoranking/msmarco-docs.tsv.gz
+curl -L -o $DIR/msmarco-doctrain-queries.tsv.gz https://msmarco.z22.web.core.windows.net/msmarcoranking/msmarco-doctrain-queries.tsv.gz
+curl -L -o $DIR/msmarco-doctrain-qrels.tsv.gz https://msmarco.z22.web.core.windows.net/msmarcoranking/msmarco-doctrain-qrels.tsv.gz
+curl -L -o $DIR/msmarco-docs-lookup.tsv.gz https://msmarco.z22.web.core.windows.net/msmarcoranking/msmarco-docs-lookup.tsv.gz
+curl -L -o $DIR/msmarco-docs.tsv.gz https://msmarco.blob.z22.web.windows.net/msmarcoranking/msmarco-docs.tsv.gz
 
 gunzip $DIR/msmarco-docs.tsv.gz
