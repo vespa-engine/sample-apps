9cda329c17e84b37235283122c81ce77d83cbdbb
leandro <leandro@vespa.ai>
2024-10-03 13:12:38 +0200

    Simplify main

--- visual-retrieval-colpali/main.py
+++ visual-retrieval-colpali/main.py
@@ -5,45 +5,25 @@ from ui.home import Home
 from ui.layout import Layout
 from ui.search import Search
 
-highlight_theme_link = Link(
-    id='highlight-theme',
-    rel="stylesheet",
-    href=""
-)
-
-theme_script = Script('''
-    (function() {
-        function getPreferredTheme() {
-            if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-                return 'dark';
-            }
-            return 'light';
-        }
-
-        function syncHighlightTheme() {
-            const link = document.getElementById('highlight-theme');
-            const preferredTheme = getPreferredTheme();
-            link.href = preferredTheme === 'dark' ? 
-                'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/github-dark.min.css' :
-                'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/github.min.css';
-        }
-
-        // Apply the correct theme immediately
-        syncHighlightTheme();
-
-        // Observe changes in the 'dark' class on the <html> element
-        const observer = new MutationObserver(syncHighlightTheme);
-        observer.observe(document.documentElement, { attributes: true, attributeFilter: ['class'] });
-    })();
-''')
-
+highlight_js_theme_link = Link(id='highlight-theme', rel="stylesheet", href="")
+highlight_js_theme = Script(src="/static/js/highlightjs-theme.js")
 highlight_js = HighlightJS(langs=['python', 'javascript', 'java', 'json', 'xml'], dark="github-dark", light="github")
 
 app, rt = fast_app(
-    hdrs=(ShadHead(tw_cdn=False, theme_handle=True), highlight_js, highlight_theme_link, theme_script),
+    htmlkw={'cls': "h-full"},
     pico=False,
-    htmlkw={'cls': "h-full"}
+    hdrs=(
+        ShadHead(tw_cdn=False, theme_handle=True),
+        highlight_js,
+        highlight_js_theme_link,
+        highlight_js_theme
+    ),
 )
+
+
+@rt("/static/{filepath:path}")
+def serve_static(filepath: str):
+    return FileResponse(f'./static/{filepath}')
 
 
 @rt("/")
