fc14584fde02e0935199e110d8ed2db80baacc33
thomasht86 <thomas@vespa.ai>
2024-10-04 13:24:18 +0200

    clean and refactor

--- visual-retrieval-colpali/backend/colpali.py
+++ visual-retrieval-colpali/backend/colpali.py
@@ -6,6 +6,10 @@ import numpy as np
 from typing import cast
 import pprint
 from pathlib import Path
+import base64
+from io import BytesIO
+from typing import Union, Tuple
+import matplotlib
 
 from colpali_engine.models import ColPali, ColPaliProcessor
 from colpali_engine.utils.torch_utils import get_torch_device
@@ -19,12 +23,14 @@ from vidore_benchmark.utils.image_utils import scale_image
 from vespa.application import Vespa
 from vespa.io import VespaQueryResponse
 
+matplotlib.use("Agg")
+
 MAX_QUERY_TERMS = 64
 OUTPUT_DIR = Path(__file__).parent.parent / "output" / "sim_maps"
 OUTPUT_DIR.mkdir(exist_ok=True)
 
 
-def load_model():
+def load_model() -> Tuple[ColPali, ColPaliProcessor]:
     model_name = "vidore/colpali-v1.2"
 
     device = get_torch_device("auto")
@@ -55,37 +61,24 @@ def load_vit_config(model):
 dummy_image = Image.new("RGB", (448, 448), (255, 255, 255))
 
 
-def get_token_dict_from_query(processor, device, query):
-    # We should refactor to only do the query inference once
-    # But we need the map of tokens and their indices to choose the token
-    # Preprocess inputs
-    input_text_processed = processor.process_queries([query]).to(device)
-    with torch.no_grad():
-        output_text = model.forward(**input_text_processed)
-    # Use this cell output to choose a token using its index
-    query_tokens = processor.tokenizer.tokenize(
-        processor.decode(input_text_processed.input_ids[0])
-    )
-    # reverse key, values in dictionary
-    return {val: idx for idx, val in query_tokens.items()}
-
-
-def gen_similarity_map(model, processor, device, vit_config, query, image=None):
+def gen_similarity_map(
+    model, processor, device, vit_config, query, image: Union[Path, str]
+):
     # Should take in the b64 image from Vespa query result
     # And possibly the tensor representing the output_image
-    if image is None:
-        # Use the default image
-        image_filepath = (
-            Path(__file__).parent.parent
-            / "static"
-            / "assets"
-            / "ConocoPhillips Sustainability Highlights - Nature (24-0976).png"
-        )
-        query = "How many percent of source water is fresh water?"
-        image = Image.open(image_filepath)
-    else:
-        # image is b64 image
-        image = Image.open(image)
+    if isinstance(image, Path):
+        # image is a file path
+        try:
+            image = Image.open(image)
+        except Exception as e:
+            raise ValueError(f"Failed to open image from path: {e}")
+    elif isinstance(image, str):
+        # image is b64 string
+        try:
+            image = Image.open(BytesIO(base64.b64decode(image)))
+        except Exception as e:
+            raise ValueError(f"Failed to open image from b64: {e}")
+
     # Preview the image
     scale_image(image, 512)
     # Preprocess inputs
@@ -143,6 +136,19 @@ def gen_similarity_map(model, processor, device, vit_config, query, image=None):
         image_resolution=vit_config.resolution,
         similarity_map=similarity_map_normalized[0, token_idx, :, :],
     )
+    ax = annotate_plot(ax, selected_token)
+    return fig, ax
+
+
+def save_figure(fig, filename: str = "similarity_map.png"):
+    fig.savefig(
+        OUTPUT_DIR / filename,
+        bbox_inches="tight",
+        pad_inches=0,
+    )
+
+
+def annotate_plot(ax, query, selected_token):
     # Add the query text
     ax.set_title(query, fontsize=18)
     # Add annotation with selected token
@@ -156,23 +162,104 @@ def gen_similarity_map(model, processor, device, vit_config, query, image=None):
         color="black",
         bbox=dict(boxstyle="round,pad=0.3", fc="white", ec="black", lw=1),
     )
+    return ax
 
-    fig.savefig(
-        OUTPUT_DIR / "similarity_map.png",
-        bbox_inches="tight",
-        pad_inches=0,
+
+def gen_similarity_map_new(
+    processor: ColPaliProcessor,
+    model: ColPali,
+    device,
+    vit_config,
+    query: str,
+    query_embs: torch.Tensor,
+    token_idx_map: dict,
+    token_to_show: str,
+    image: Union[Path, str],
+):
+    if isinstance(image, Path):
+        # image is a file path
+        try:
+            image = Image.open(image)
+        except Exception as e:
+            raise ValueError(f"Failed to open image from path: {e}")
+    elif isinstance(image, str):
+        # image is b64 string
+        try:
+            image = Image.open(BytesIO(base64.b64decode(image)))
+        except Exception as e:
+            raise ValueError(f"Failed to open image from b64: {e}")
+    token_idx = token_idx_map[token_to_show]
+    print(f"Selected token: `{token_to_show}`")
+    # strip whitespace
+    # Preview the image
+    # scale_image(image, 512)
+    # Preprocess inputs
+    input_image_processed = processor.process_images([image]).to(device)
+    # Forward passes
+    with torch.no_grad():
+        output_image = model.forward(**input_image_processed)
+    # output_image is the tensor that we could get from the Vespa query
+    # Print shape of output_text and output_image
+    # Output image shape: torch.Size([1, 1030, 128])
+    # Remove the special tokens from the output
+    print(f"Output image shape before dim: {output_image.shape}")
+    output_image = output_image[
+        :, : processor.image_seq_length, :
+    ]  # (1, n_patches_x * n_patches_y, dim)
+    print(f"Output image shape after dim: {output_image.shape}")
+    # Rearrange the output image tensor to explicitly represent the 2D grid of patches
+    output_image = rearrange(
+        output_image,
+        "b (h w) c -> b h w c",
+        h=vit_config.n_patch_per_dim,
+        w=vit_config.n_patch_per_dim,
+    )  # (1, n_patches_x, n_patches_y, dim)
+    # Get the similarity map
+    print(f"Query embs shape: {query_embs.shape}")
+    # Add 1 extra dim to start of query_embs
+    query_embs = query_embs.unsqueeze(0).to(device)
+    print(f"Output image shape: {output_image.shape}")
+    similarity_map = torch.einsum(
+        "bnk,bijk->bnij", query_embs, output_image
+    )  # (1, query_tokens, n_patches_x, n_patches_y)
+    print(f"Similarity map shape: {similarity_map.shape}")
+    # Normalize the similarity map
+    similarity_map_normalized = normalize_similarity_map_per_query_token(
+        similarity_map
+    )  # (1, query_tokens, n_patches_x, n_patches_y)
+    print(f"Similarity map normalized shape: {similarity_map_normalized.shape}")
+    # Use this cell output to choose a token using its index
+    input_image_square = image.resize((vit_config.resolution, vit_config.resolution))
+
+    # Plot the similarity map
+    fig, ax = plot_similarity_heatmap(
+        input_image_square,
+        patch_size=vit_config.patch_size,
+        image_resolution=vit_config.resolution,
+        similarity_map=similarity_map_normalized[0, token_idx, :, :],
     )
-    return similarity_map_normalized
+    ax = annotate_plot(ax, query, token_to_show)
+    # save the figure
+    save_figure(fig, f"similarity_map_{token_to_show}.png")
+    return fig, ax
 
 
-def process_queries(processor, queries, image):
-    inputs = processor(
-        images=[image] * len(queries), text=queries, return_tensors="pt", padding=True
-    )
-    return inputs
+def get_query_embeddings_and_token_map(
+    processor, model, query, image
+) -> Tuple[torch.Tensor, dict]:
+    inputs = processor.process_queries([query]).to(model.device)
+    with torch.no_grad():
+        embeddings_query = model(**inputs)
+        q_emb = embeddings_query.to("cpu")[0]  # Extract the single embedding
+    # Use this cell output to choose a token using its index
+    query_tokens = processor.tokenizer.tokenize(processor.decode(inputs.input_ids[0]))
+    # reverse key, values in dictionary
+    print(query_tokens)
+    token_to_idx = {val: idx for idx, val in enumerate(query_tokens)}
+    return q_emb, token_to_idx
 
 
-def display_query_results(query, response, hits=5):
+def format_query_results(query, response, hits=5) -> dict:
     query_time = response.json.get("timing", {}).get("searchtime", -1)
     query_time = round(query_time, 2)
     count = response.json.get("root", {}).get("fields", {}).get("totalCount", 0)
@@ -181,84 +268,134 @@ def display_query_results(query, response, hits=5):
     return response.json
 
 
-async def query_vespa_default(app: Vespa, query, q):
+async def query_vespa_default(
+    app: Vespa,
+    query: str,
+    q_emb: torch.Tensor,
+    hits: int = 3,
+    timeout: str = "10s",
+    **kwargs,
+) -> dict:
     async with app.asyncio(connections=1, total_timeout=120) as session:
-        query_embedding = {k: v.tolist() for k, v in enumerate(q)}
+        query_embedding = format_q_embs(q_emb)
         response: VespaQueryResponse = await session.query(
             body={
                 "yql": "select id,title,url,image,page_number from pdf_page where userQuery();",
                 "ranking": "default",
                 "query": query,
-                "timeout": "10s",
-                "hits": 3,
+                "timeout": timeout,
+                "hits": hits,
                 "input.query(qt)": query_embedding,
                 "presentation.timing": True,
+                **kwargs,
             },
         )
         assert response.is_successful(), response.json
-    return display_query_results(query, response)
+    return format_query_results(query, response)
 
 
-async def query_vespa_nearest_neighbor(app: Vespa, query, q):
-    target_hits_per_query_tensor = 20  # Hyperparameter for speed vs. accuracy
+def float_to_binary_embedding(float_query_embedding: dict) -> dict:
+    binary_query_embeddings = {}
+    for k, v in float_query_embedding.items():
+        binary_vector = (
+            np.packbits(np.where(np.array(v) > 0, 1, 0)).astype(np.int8).tolist()
+        )
+        binary_query_embeddings[k] = binary_vector
+        if len(binary_query_embeddings) >= MAX_QUERY_TERMS:
+            print(f"Warning: Query has more than {MAX_QUERY_TERMS} terms. Truncating.")
+            break
+    return binary_query_embeddings
+
+
+def create_nn_query_strings(
+    binary_query_embeddings: dict, target_hits_per_query_tensor: int = 20
+) -> Tuple[str, dict]:
+    # Query tensors for nearest neighbor calculations
+    nn_query_dict = {}
+    for i in range(len(binary_query_embeddings)):
+        nn_query_dict[f"input.query(rq{i})"] = binary_query_embeddings[i]
+    nn = " OR ".join(
+        [
+            f"({{targetHits:{target_hits_per_query_tensor}}}nearestNeighbor(embedding,rq{i}))"
+            for i in range(len(binary_query_embeddings))
+        ]
+    )
+    return nn, nn_query_dict
+
+
+def format_q_embs(q_embs: torch.Tensor) -> dict:
+    float_query_embedding = {k: v.tolist() for k, v in enumerate(q_embs)}
+    return float_query_embedding
+
+
+async def query_vespa_nearest_neighbor(
+    app: Vespa,
+    query: str,
+    q_emb: torch.Tensor,
+    target_hits_per_query_tensor: int = 20,
+    hits: int = 3,
+    timeout: str = "10s",
+    **kwargs,
+) -> dict:
+    # Hyperparameter for speed vs. accuracy
     async with app.asyncio(connections=1, total_timeout=180) as session:
-        float_query_embedding = {k: v.tolist() for k, v in enumerate(q)}
-        binary_query_embeddings = {}
-        for k, v in float_query_embedding.items():
-            binary_vector = (
-                np.packbits(np.where(np.array(v) > 0, 1, 0)).astype(np.int8).tolist()
-            )
-            binary_query_embeddings[k] = binary_vector
-            if len(binary_query_embeddings) >= MAX_QUERY_TERMS:
-                print(
-                    f"Warning: Query has more than {MAX_QUERY_TERMS} terms. Truncating."
-                )
-                break
+        float_query_embedding = format_q_embs(q_emb)
+        binary_query_embeddings = float_to_binary_embedding(float_query_embedding)
 
         # Mixed tensors for MaxSim calculations
         query_tensors = {
             "input.query(qtb)": binary_query_embeddings,
             "input.query(qt)": float_query_embedding,
         }
-
-        # Query tensors for nearest neighbor calculations
-        for i in range(len(binary_query_embeddings)):
-            query_tensors[f"input.query(rq{i})"] = binary_query_embeddings[i]
-        nn = " OR ".join(
-            [
-                f"({{targetHits:{target_hits_per_query_tensor}}}nearestNeighbor(embedding,rq{i}))"
-                for i in range(len(binary_query_embeddings))
-            ]
+        nn_string, nn_query_dict = create_nn_query_strings(
+            binary_query_embeddings, target_hits_per_query_tensor
         )
-
+        query_tensors.update(nn_query_dict)
         response: VespaQueryResponse = await session.query(
             body={
                 **query_tensors,
                 "presentation.timing": True,
-                "yql": f"select id,title, url, image, page_number from pdf_page where {nn}",
+                "yql": f"select id,title, url, image, page_number from pdf_page where {nn_string}",
                 "ranking.profile": "retrieval-and-rerank",
-                "timeout": "10s",
-                "hits": 3,
+                "timeout": timeout,
+                "hits": hits,
+                **kwargs,
             },
         )
         assert response.is_successful(), response.json
-    return display_query_results(query, response)
+    return format_query_results(query, response)
 
 
 async def get_result_from_query(
     app: Vespa, processor: ColPaliProcessor, model: ColPali, query: str, nn=False
 ):
-    # Process the single query
-    batch_query = process_queries(processor, [query], dummy_image)
-    with torch.no_grad():
-        batch_query = {k: v.to(model.device) for k, v in batch_query.items()}
-        embeddings_query = model(**batch_query)
-        q = embeddings_query.to("cpu")[0]  # Extract the single embedding
+    # Get the query embeddings and token map
     print(query)
+    q_embs, token_to_idx = get_query_embeddings_and_token_map(
+        processor, model, query, dummy_image
+    )
+    print(token_to_idx)
+    # Use the token map to choose a token randomly for now
+    # Dynamically select a token containing 'water'
+    token = next((t for t in token_to_idx if "water" in t), None)
+    if token is None:
+        raise ValueError("Token containing 'water' not found")
     if nn:
-        result = await query_vespa_nearest_neighbor(app, query, q)
+        result = await query_vespa_nearest_neighbor(app, query, q_embs)
     else:
-        result = await query_vespa_default(app, query, q)
+        result = await query_vespa_default(app, query, q_embs)
+    img = result["root"]["children"][0]["fields"]["image"]
+    fig, ax = gen_similarity_map_new(
+        processor,
+        model,
+        model.device,
+        load_vit_config(model),
+        query,
+        q_embs,
+        token_to_idx,
+        token,
+        img,
+    )
     return result
 
 
@@ -328,8 +465,16 @@ def get_result_dummy(query: str, nn: bool = False):
 if __name__ == "__main__":
     model, processor = load_model()
     vit_config = load_vit_config(model)
-    query = "Quelle partie de la production pétrolière du Kazakhstan provient de champs en mer ?"
-    gen_similarity_map(model, processor, model.device, vit_config, query=query)
+    query = "How many percent of source water is fresh water?"
+    image_filepath = (
+        Path(__file__).parent.parent
+        / "static"
+        / "assets"
+        / "ConocoPhillips Sustainability Highlights - Nature (24-0976).png"
+    )
+    gen_similarity_map(
+        model, processor, model.device, vit_config, query=query, image=image_filepath
+    )
     result = get_result_dummy("dummy query")
     print(result)
     print("Done")
