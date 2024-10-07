fa6b5c0f6c81dd06aa841892b68e0d1c79330098
thomasht86 <thomas@vespa.ai>
2024-10-07 14:23:29 +0200

    just printing out sim_maps (and formatting)

--- visual-retrieval-colpali/frontend/app.py
+++ visual-retrieval-colpali/frontend/app.py
@@ -35,16 +35,13 @@ def SearchBox(with_border=False, query_value=""):
                 value=query_value,
                 id="search-input",
                 cls="text-base pl-10 border-transparent ring-offset-transparent ring-0 focus-visible:ring-transparent",
-                style='font-size: 1rem',
-                autofocus=True
+                style="font-size: 1rem",
+                autofocus=True,
             ),
-            cls="relative"
+            cls="relative",
         ),
         Div(
-            Span(
-                "controls",
-                cls="text-muted-foreground"
-            ),
+            Span("controls", cls="text-muted-foreground"),
             Button(
                 Lucide(icon="arrow-right", size="21"),
                 size="sm",
@@ -52,7 +49,7 @@ def SearchBox(with_border=False, query_value=""):
                 data_button="search-button",
                 disabled=True,
             ),
-            cls="flex justify-between"
+            cls="flex justify-between",
         ),
         check_input_script(),
         action=f"/search?query={quote_plus(query_value)}",  # This takes the user to /search with the loading message
@@ -70,7 +67,7 @@ def SampleQueries():
     sample_queries = [
         "What is the future of energy storage?",
         "What is sustainable energy?",
-        "How to reduce carbon emissions?"
+        "How to reduce carbon emissions?",
     ]
 
     query_badges = []
@@ -79,15 +76,17 @@ def SampleQueries():
             A(
                 Badge(
                     Div(
-                        Lucide(icon="text-search", size="18", cls="text-muted-foreground"),
+                        Lucide(
+                            icon="text-search", size="18", cls="text-muted-foreground"
+                        ),
                         Span(query, cls="text-base font-normal"),
                         cls="flex gap-2 items-center",
                     ),
                     variant="outline",
-                    cls="text-base font-normal text-muted-foreground"
+                    cls="text-base font-normal text-muted-foreground",
                 ),
                 href=f"/search?query={quote_plus(query)}",
-                cls="no-underline"
+                cls="no-underline",
             )
         )
 
@@ -98,13 +97,13 @@ def Hero():
     return Div(
         H1(
             "Vespa.Ai + ColPali",
-            cls="text-5xl md:text-7xl font-bold tracking-wide md:tracking-wider bg-clip-text text-transparent bg-gradient-to-r from-black to-gray-700 dark:from-white dark:to-gray-300 animate-fade-in"
+            cls="text-5xl md:text-7xl font-bold tracking-wide md:tracking-wider bg-clip-text text-transparent bg-gradient-to-r from-black to-gray-700 dark:from-white dark:to-gray-300 animate-fade-in",
         ),
         P(
             "Efficient Document Retrieval with Vision Language Models",
-            cls="text-lg md:text-2xl text-muted-foreground md:tracking-wide"
+            cls="text-lg md:text-2xl text-muted-foreground md:tracking-wide",
         ),
-        cls="grid gap-5 text-center"
+        cls="grid gap-5 text-center",
     )
 
 
@@ -114,23 +113,25 @@ def Home():
             Hero(),
             SearchBox(with_border=True),
             SampleQueries(),
-            cls="grid gap-8 -mt-[34vh]"
+            cls="grid gap-8 -mt-[34vh]",
         ),
-        cls="grid w-full h-full max-w-screen-md items-center gap-4 mx-auto"
+        cls="grid w-full h-full max-w-screen-md items-center gap-4 mx-auto",
     )
 
 
 def Search(request, search_results=[]):
-    query_value = request.query_params.get('query', '').strip()
+    query_value = request.query_params.get("query", "").strip()
 
     return Div(
         Div(
-            SearchBox(query_value=query_value),  # Pass the query value to pre-fill the SearchBox
+            SearchBox(
+                query_value=query_value
+            ),  # Pass the query value to pre-fill the SearchBox
             Div(
                 LoadingMessage(),  # Show the loading message initially
-                id="search-results"  # This will be replaced by the search results
+                id="search-results",  # This will be replaced by the search results
             ),
-            cls="grid"
+            cls="grid",
         ),
         cls="grid",
     )
@@ -140,7 +141,7 @@ def LoadingMessage():
     return Div(
         P("Loading... Please wait.", cls="text-base text-center"),
         cls="p-10 text-center text-muted-foreground",
-        id="loading-indicator"
+        id="loading-indicator",
     )
 
 
@@ -149,37 +150,44 @@ def SearchResult(results=[]):
         return Div(
             P(
                 "No results found for your query.",
-                cls="text-muted-foreground text-base text-center"
+                cls="text-muted-foreground text-base text-center",
             ),
-            cls="grid p-10"
+            cls="grid p-10",
         )
 
     # Otherwise, display the search results
     result_items = []
     for result in results:
-        fields = result['fields']  # Extract the 'fields' part of each result
-        base64_image = f"data:image/jpeg;base64,{fields['image']}"  # Format base64 image
-
+        fields = result["fields"]  # Extract the 'fields' part of each result
+        base64_image = (
+            f"data:image/jpeg;base64,{fields['image']}"  # Format base64 image
+        )
+        # Print the fields that start with 'sim_map'
+        for key, value in fields.items():
+            if key.startswith("sim_map"):
+                print(f"{key}")
         result_items.append(
             Div(
                 Div(
-                    Img(src=base64_image, alt=fields['title'], cls='max-w-full h-auto'),
-                    cls="bg-background px-3 py-5"
+                    Img(src=base64_image, alt=fields["title"], cls="max-w-full h-auto"),
+                    cls="bg-background px-3 py-5",
                 ),
                 Div(
                     Div(
-                        H2(fields['title'], cls="text-xl font-semibold"),
-                        P(fields['url'], cls="text-muted-foreground"),  # Use the URL as the description
-                        cls="text-sm grid gap-y-4"
+                        H2(fields["title"], cls="text-xl font-semibold"),
+                        P(
+                            fields["text"], cls="text-muted-foreground"
+                        ),  # Use the URL as the description
+                        cls="text-sm grid gap-y-4",
                     ),
-                    cls="bg-background px-3 py-5"
+                    cls="bg-background px-3 py-5",
                 ),
-                cls="grid grid-cols-subgrid col-span-2"
+                cls="grid grid-cols-subgrid col-span-2",
             )
         )
 
     return Div(
         *result_items,
         cls="grid grid-cols-2 gap-px bg-border",
-        id="search-results"  # This will be the target for HTMX updates
+        id="search-results",  # This will be the target for HTMX updates
     )
