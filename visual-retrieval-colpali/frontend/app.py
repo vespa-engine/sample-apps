121b89d8ab9cdbef5c934487e4f1cf3cf9885557
Leandro Alves <ldalves@gmail.com>
2024-10-11 10:53:25 +0200

    Update visual-retrieval-colpali/frontend/app.py
    Co-authored-by: Andreas Eriksen <andreer@vespa.ai>

--- visual-retrieval-colpali/frontend/app.py
+++ visual-retrieval-colpali/frontend/app.py
@@ -279,7 +279,7 @@ def SearchResult(results: list, query_id: Optional[str] = None):
                         ),
                         Div(
                             Badge(
-                                f"Relevance score: {result['relevance']:.2f}",
+                                f"Relevance score: {result['relevance']:.4f}",
                                 cls="flex gap-1.5 items-center justify-center",
                             ),
                         ),
