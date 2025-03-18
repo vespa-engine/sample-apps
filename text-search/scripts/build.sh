4239aeaa28aea0457e1f3820d29e5422a94e51e1
Marlon Saglia <marlon@vespa.ai>
2025-03-18 14:12:46 +0100

    feat: add deployment workflow to deploy sample apps to Vespa Cloud

---
+++ text-search/scripts/build.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+#
+
+set -euo pipefail
+if [[  -n "${DEBUG:-}" ]]; then
+  set -x
+fi
+
+
+main() {
+  local APP_DIR
+
+  APP_DIR="$(realpath "$(dirname "$0")/..")"
+
+  echo "### Setup '${APP_DIR}'  ###"
+  pushd "$APP_DIR" >/dev/null
+
+  mkdir -p dist/
+  echo "Generating:"
+
+  # Convert the documents to JSONL format
+  echo '  * dist/documents.jsonl'
+  bash scripts/convert-msmarco.sh
+  mkdir -p dataset/
+  cp ext/vespa.json dataset/documents.jsonl
+
+  (
+    echo '  * dist/application.zip'
+    cp README.md app/
+    cd app && zip --recurse-paths --quiet ../dist/application.zip ./
+  )
+
+  popd >/dev/null
+  echo "### Finished building sample application ###"
+}
+
+main "$@"
