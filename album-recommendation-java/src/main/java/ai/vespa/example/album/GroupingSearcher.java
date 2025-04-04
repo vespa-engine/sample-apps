9deeecae149940dc174829f17ed1b32a17a94d9b
vadimkatsel <vadim.katsel@gmail.com>
2025-04-04 12:47:21 +0200

    Add comments

--- album-recommendation-java/src/main/java/ai/vespa/example/album/GroupingSearcher.java
+++ album-recommendation-java/src/main/java/ai/vespa/example/album/GroupingSearcher.java
@@ -1,3 +1,4 @@
+// Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 package ai.vespa.example.album;
 
 import com.yahoo.search.Query;
@@ -12,10 +13,32 @@ import com.yahoo.search.searchchain.Execution;
 
 import java.util.logging.Logger;
 
+/**
+ * A searcher which adds an grouping to queries.
+ *
+ * See https://docs.vespa.ai/en/searcher-development.html
+ * See https://docs.vespa.ai/en/grouping.html#global-limit-for-grouping-queries
+ */
 public class GroupingSearcher extends Searcher {
     public final static String GROUPING_FIELD = "year";
     private static final Logger log = Logger.getLogger(MetalSearcher.class.getName());
 
+    /**
+     * Performs a search and post-processes the result with a grouping operation based on a specific attribute.
+     * <p>
+     * This method uses Vespa to count the number of results per unique value of
+     * the {@code GROUPING_FIELD}. It constructs a grouping request that groups hits by the specified field
+     * and counts how many documents fall into each group. These aggregated counts are then added back as
+     * synthetic hits in the final {@link Result} object.
+     * </p>
+     *
+     * Args:
+     *     query (Query): The search query.
+     *     execution (Execution): The Vespa execution context used to perform the search.
+     *
+     * Returns:
+     *     Result: The original search result with additional hits representing grouped counts by the given field.
+     */
     @Override
     public Result search(Query query, Execution execution) {
         GroupingRequest grpRequest = GroupingRequest.newInstance(query);
