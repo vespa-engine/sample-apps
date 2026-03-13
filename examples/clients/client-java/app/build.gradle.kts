49142653f13508efd552e34c55361866b403a7e8
Magnus Eide-Fredriksen <magnuseifr@gmail.com>
2026-03-13 16:41:25 +0100

    chore: update jetty version

--- examples/clients/client-java/app/build.gradle.kts
+++ examples/clients/client-java/app/build.gradle.kts
@@ -12,8 +12,8 @@ dependencies {
     implementation(libs.guava)
 
     implementation("io.github.hakky54:ayza-for-pem:10.0.3")
-    implementation("org.eclipse.jetty:jetty-client:12.0.14")
-    implementation("org.eclipse.jetty.http2:jetty-http2-client-transport:12.0.14")
+    implementation("org.eclipse.jetty:jetty-client:12.1.7")
+    implementation("org.eclipse.jetty.http2:jetty-http2-client-transport:12.1.7")
     implementation("org.slf4j:slf4j-simple:2.0.17")
     implementation("commons-cli:commons-cli:1.11.0")
     implementation("com.yahoo.vespa:vespa-feed-client:8.643.19");
